a0b3f5a219b2e595edc281fc80d9642d1ed49dfb
Julian Krzefski <krzefski@heptacom.de>
2020-07-23 15:58:44 +0200

    $ make csfix

--- src/Explore/ExploreService.php
+++ src/Explore/ExploreService.php
@@ -55,6 +55,6 @@ class ExploreService implements ExploreServiceInterface
             $types[$explorer->supports()] = true;
         }
 
-        return array_keys($types);
+        return \array_keys($types);
     }
 }
