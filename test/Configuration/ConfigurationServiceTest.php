44169ef0778a0a3efced4cac44c2037cf63fd900
Joshua Behrens <behrens@heptacom.de>
2020-07-30 14:16:51 +0200

    Add test for ConfigurationService SPBCC-11

---
+++ test/Configuration/ConfigurationServiceTest.php
@@ -0,0 +1,73 @@
+<?php declare(strict_types=1);
+
+namespace Heptacom\HeptaConnect\Core\Test\Configuration;
+
+use Heptacom\HeptaConnect\Core\Configuration\ConfigurationService;
+use Heptacom\HeptaConnect\Core\Portal\Contract\PortalRegistryInterface;
+use Heptacom\HeptaConnect\Portal\Base\Portal\Contract\PortalContract;
+use Heptacom\HeptaConnect\Portal\Base\Portal\Contract\PortalExtensionContract;
+use Heptacom\HeptaConnect\Portal\Base\Portal\PortalExtensionCollection;
+use Heptacom\HeptaConnect\Portal\Base\StorageKey\Contract\PortalNodeKeyInterface;
+use PHPUnit\Framework\TestCase;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+/**
+ * @covers \Heptacom\HeptaConnect\Core\Configuration\ConfigurationService
+ */
+class ConfigurationServiceTest extends TestCase
+{
+    public function testConfigurationTemplateLoading(): void
+    {
+        $registry = $this->createMock(PortalRegistryInterface::class);
+        $registry->method('getPortal')->willReturn(new class extends PortalContract {
+            public function getConfigurationTemplate(): OptionsResolver
+            {
+                return parent::getConfigurationTemplate()
+                    ->setDefined(['limit'])
+                    ->setDefault('limit', 100);
+            }
+        });
+        $registry->method('getPortalExtensions')->willReturn(new PortalExtensionCollection());
+        $configService = new ConfigurationService($registry);
+
+        $config = $configService->getPortalNodeConfiguration($this->createMock(PortalNodeKeyInterface::class));
+        static::assertArrayHasKey('limit', $config);
+        static::assertEquals(100, $config['limit']);
+    }
+
+    public function testConfigurationTemplateLoadingWithExtensionOverrides(): void
+    {
+        $registry = $this->createMock(PortalRegistryInterface::class);
+        $registry->method('getPortal')->willReturn(new class extends PortalContract {
+            public function getConfigurationTemplate(): OptionsResolver
+            {
+                return parent::getConfigurationTemplate()
+                    ->setDefined(['limit'])
+                    ->setDefault('limit', 100);
+            }
+        });
+        $registry->method('getPortalExtensions')->willReturn(new PortalExtensionCollection([
+            new class extends PortalExtensionContract {
+                public function supports(): string
+                {
+                    return PortalContract::class;
+                }
+
+                public function extendConfiguration(OptionsResolver $template): OptionsResolver
+                {
+                    return parent::extendConfiguration($template)
+                        ->setDefined(['offset'])
+                        ->setDefault('offset', 1000)
+                        ->setDefault('limit', 200);
+                }
+            }
+        ]));
+        $configService = new ConfigurationService($registry);
+
+        $config = $configService->getPortalNodeConfiguration($this->createMock(PortalNodeKeyInterface::class));
+        static::assertArrayHasKey('limit', $config);
+        static::assertEquals(200, $config['limit']);
+        static::assertArrayHasKey('offset', $config);
+        static::assertEquals(1000, $config['offset']);
+    }
+}
