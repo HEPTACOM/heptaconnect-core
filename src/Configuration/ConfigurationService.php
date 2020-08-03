92bbd31eec07c056d286b7c8d16e3164b49fd312
Joshua Behrens <behrens@heptacom.de>
2020-08-03 13:11:11 +0200

    Add configuration setter in ConfigurationService SPBCC-11

--- src/Configuration/ConfigurationService.php
+++ src/Configuration/ConfigurationService.php
@@ -7,6 +7,7 @@ use Heptacom\HeptaConnect\Core\Portal\Contract\PortalRegistryInterface;
 use Heptacom\HeptaConnect\Portal\Base\Portal\Contract\PortalContract;
 use Heptacom\HeptaConnect\Portal\Base\StorageKey\Contract\PortalNodeKeyInterface;
 use Heptacom\HeptaConnect\Storage\Base\Contract\StorageInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
 
 class ConfigurationService implements ConfigurationServiceInterface
 {
@@ -22,9 +23,57 @@ class ConfigurationService implements ConfigurationServiceInterface
 
     public function getPortalNodeConfiguration(PortalNodeKeyInterface $portalNodeKey): ?array
     {
+        $template = $this->getMergedConfigurationTemplate($portalNodeKey);
+
+        if (\is_null($template)) {
+            return null;
+        }
+
+        return $template->resolve($this->storage->getConfiguration($portalNodeKey));
+    }
+
+    public function setPortalNodeConfiguration(PortalNodeKeyInterface $portalNodeKey, ?array $configuration): void
+    {
+        $template = $this->getMergedConfigurationTemplate($portalNodeKey);
+
+        if (\is_null($template)) {
+            return;
+        }
+
+        $data = $this->storage->getConfiguration($portalNodeKey);
+        $data = $this->removeStorageKeysWhenValueIsNull($data, $configuration ?? []);
+        $data = \is_null($configuration) ? [] : \array_replace_recursive($data, $configuration);
+
+        $template->resolve($data);
+        $this->storage->setConfiguration($portalNodeKey, $data);
+    }
+
+    /**
+     * @TODO extract for easier testing
+     */
+    private function removeStorageKeysWhenValueIsNull(array $editable, array $nullArray): array
+    {
+        foreach ($nullArray as $key => $value) {
+            if (\is_array($key) && \array_key_exists($key, $editable)) {
+                $editable[$key] = $this->removeStorageKeysWhenValueIsNull($editable[$key], $value);
+                continue;
+            }
+
+            if (!\is_null($value)) {
+                continue;
+            }
+
+            unset($editable[$key]);
+        }
+
+        return $editable;
+    }
+
+    private function getMergedConfigurationTemplate(PortalNodeKeyInterface $portalNodeKey): ?OptionsResolver
+    {
         $portal = $this->portalRegistry->getPortal($portalNodeKey);
 
-        if (!$portal instanceof PortalContract) {
+        if (!($portal instanceof PortalContract)) {
             return null;
         }
 
@@ -35,6 +84,6 @@ class ConfigurationService implements ConfigurationServiceInterface
             $template = $extension->extendConfiguration($template);
         }
 
-        return $template->resolve($this->storage->getConfiguration($portalNodeKey));
+        return $template;
     }
 }
--- src/Configuration/Contract/ConfigurationServiceInterface.php
+++ src/Configuration/Contract/ConfigurationServiceInterface.php
@@ -7,4 +7,6 @@ use Heptacom\HeptaConnect\Portal\Base\StorageKey\Contract\PortalNodeKeyInterface
 interface ConfigurationServiceInterface
 {
     public function getPortalNodeConfiguration(PortalNodeKeyInterface $portalNodeKey): ?array;
+
+    public function setPortalNodeConfiguration(PortalNodeKeyInterface $portalNodeKey, ?array $configuration): void;
 }
