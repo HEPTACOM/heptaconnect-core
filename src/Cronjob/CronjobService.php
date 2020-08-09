93f6b77195550d7507476cec5364a22a4a8f4cdf
Joshua Behrens <behrens@heptacom.de>
2020-08-09 04:06:22 +0200

    Move CronjobService out of Component namespace into Cronjob namespace SPBCC-11

--- src/Cronjob/CronjobService.php
+++ src/Cronjob/CronjobService.php
@@ -1,6 +1,6 @@
 <?php declare(strict_types=1);
 
-namespace Heptacom\HeptaConnect\Core\Component\Cronjob;
+namespace Heptacom\HeptaConnect\Core\Cronjob;
 
 use Cron\CronExpression;
 use Heptacom\HeptaConnect\Portal\Base\Cronjob\Contract\CronjobInterface;
