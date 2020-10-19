2bf141d6056a64eee9c65e5833d2093666e4f380
Julian Krzefski <krzefski@heptacom.de>
2020-10-19 10:17:06 +0200

    Add todo comment

--- src/Reception/Contract/ReceiveServiceInterface.php
+++ src/Reception/Contract/ReceiveServiceInterface.php
@@ -6,5 +6,8 @@ use Heptacom\HeptaConnect\Portal\Base\Mapping\TypedMappedDatasetEntityCollection
 
 interface ReceiveServiceInterface
 {
+    /**
+     * @todo simplify saving of mappings / replace callback with return value
+     */
     public function receive(TypedMappedDatasetEntityCollection $mappedDatasetEntities, callable $saveMappings): void;
 }
