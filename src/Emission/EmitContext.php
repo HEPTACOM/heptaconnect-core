3ec9c1d3d0a36a5dcf96e225eac320b1d8a793e9
Joshua Behrens <behrens@heptacom.de>
2020-11-30 04:00:06 +0100

    Extract StatelessContextInterface from EmitContext and ReceiveContext

--- src/Emission/EmitContext.php
+++ src/Emission/EmitContext.php
@@ -2,71 +2,9 @@
 
 namespace Heptacom\HeptaConnect\Core\Emission;
 
-use Heptacom\HeptaConnect\Core\Configuration\Contract\ConfigurationServiceInterface;
-use Heptacom\HeptaConnect\Core\Mapping\Contract\MappingServiceInterface;
-use Heptacom\HeptaConnect\Core\Portal\Contract\PortalRegistryInterface;
-use Heptacom\HeptaConnect\Core\Portal\PortalStorageFactory;
+use Heptacom\HeptaConnect\Core\Portal\AbstractStatelessContext;
 use Heptacom\HeptaConnect\Portal\Base\Emission\Contract\EmitContextInterface;
-use Heptacom\HeptaConnect\Portal\Base\Mapping\Contract\MappingInterface;
-use Heptacom\HeptaConnect\Portal\Base\Parallelization\Contract\ResourceLockingContract;
-use Heptacom\HeptaConnect\Portal\Base\Parallelization\Support\ResourceLockFacade;
-use Heptacom\HeptaConnect\Portal\Base\Portal\Contract\PortalContract;
-use Heptacom\HeptaConnect\Portal\Base\Portal\Contract\PortalStorageInterface;
-use Heptacom\HeptaConnect\Portal\Base\StorageKey\Contract\PortalNodeKeyInterface;
 
-class EmitContext implements EmitContextInterface
+class EmitContext extends AbstractStatelessContext implements EmitContextInterface
 {
-    private MappingServiceInterface $mappingService;
-
-    private ConfigurationServiceInterface $configurationService;
-
-    private PortalRegistryInterface $portalRegistry;
-
-    private PortalStorageFactory $portalStorageFactory;
-
-    private ResourceLockFacade $resourceLockFacade;
-
-    public function __construct(
-        MappingServiceInterface $mappingService,
-        ConfigurationServiceInterface $configurationService,
-        PortalRegistryInterface $portalRegistry,
-        PortalStorageFactory $portalStorageFactory,
-        ResourceLockingContract $resourceLocking
-    ) {
-        $this->mappingService = $mappingService;
-        $this->configurationService = $configurationService;
-        $this->portalRegistry = $portalRegistry;
-        $this->portalStorageFactory = $portalStorageFactory;
-        $this->resourceLockFacade = new ResourceLockFacade($resourceLocking);
-    }
-
-    public function getPortalNodeKey(MappingInterface $mapping): PortalNodeKeyInterface
-    {
-        return $mapping->getPortalNodeKey();
-    }
-
-    public function getConfig(MappingInterface $mapping): ?array
-    {
-        return $this->configurationService->getPortalNodeConfiguration($mapping->getPortalNodeKey());
-    }
-
-    public function getPortal(MappingInterface $mapping): PortalContract
-    {
-        return $this->portalRegistry->getPortal($mapping->getPortalNodeKey());
-    }
-
-    public function markAsFailed(MappingInterface $mapping, \Throwable $throwable): void
-    {
-        $this->mappingService->addException($mapping, $throwable);
-    }
-
-    public function getStorage(MappingInterface $mapping): PortalStorageInterface
-    {
-        return $this->portalStorageFactory->createPortalStorage($mapping->getPortalNodeKey());
-    }
-
-    public function getResourceLocker(): ResourceLockFacade
-    {
-        return $this->resourceLockFacade;
-    }
 }
---
+++ src/Portal/AbstractStatelessContext.php
@@ -0,0 +1,71 @@
+<?php declare(strict_types=1);
+
+namespace Heptacom\HeptaConnect\Core\Portal;
+
+use Heptacom\HeptaConnect\Core\Configuration\Contract\ConfigurationServiceInterface;
+use Heptacom\HeptaConnect\Core\Mapping\Contract\MappingServiceInterface;
+use Heptacom\HeptaConnect\Core\Portal\Contract\PortalRegistryInterface;
+use Heptacom\HeptaConnect\Portal\Base\Mapping\Contract\MappingInterface;
+use Heptacom\HeptaConnect\Portal\Base\Parallelization\Contract\ResourceLockingContract;
+use Heptacom\HeptaConnect\Portal\Base\Parallelization\Support\ResourceLockFacade;
+use Heptacom\HeptaConnect\Portal\Base\Portal\Contract\PortalContract;
+use Heptacom\HeptaConnect\Portal\Base\Portal\Contract\PortalStorageInterface;
+use Heptacom\HeptaConnect\Portal\Base\Portal\Contract\StatelessContextInterface;
+use Heptacom\HeptaConnect\Portal\Base\StorageKey\Contract\PortalNodeKeyInterface;
+
+abstract class AbstractStatelessContext implements StatelessContextInterface
+{
+    private MappingServiceInterface $mappingService;
+
+    private ConfigurationServiceInterface $configurationService;
+
+    private PortalRegistryInterface $portalRegistry;
+
+    private PortalStorageFactory $portalStorageFactory;
+
+    private ResourceLockFacade $resourceLockFacade;
+
+    public function __construct(
+        MappingServiceInterface $mappingService,
+        ConfigurationServiceInterface $configurationService,
+        PortalRegistryInterface $portalRegistry,
+        PortalStorageFactory $portalStorageFactory,
+        ResourceLockingContract $resourceLocking
+    ) {
+        $this->mappingService = $mappingService;
+        $this->configurationService = $configurationService;
+        $this->portalRegistry = $portalRegistry;
+        $this->portalStorageFactory = $portalStorageFactory;
+        $this->resourceLockFacade = new ResourceLockFacade($resourceLocking);
+    }
+
+    public function getConfig(MappingInterface $mapping): ?array
+    {
+        return $this->configurationService->getPortalNodeConfiguration($mapping->getPortalNodeKey());
+    }
+
+    public function getResourceLocker(): ResourceLockFacade
+    {
+        return $this->resourceLockFacade;
+    }
+
+    public function getStorage(MappingInterface $mapping): PortalStorageInterface
+    {
+        return $this->portalStorageFactory->createPortalStorage($mapping->getPortalNodeKey());
+    }
+
+    public function getPortalNodeKey(MappingInterface $mapping): PortalNodeKeyInterface
+    {
+        return $mapping->getPortalNodeKey();
+    }
+
+    public function getPortal(MappingInterface $mapping): PortalContract
+    {
+        return $this->portalRegistry->getPortal($mapping->getPortalNodeKey());
+    }
+
+    public function markAsFailed(MappingInterface $mapping, \Throwable $throwable): void
+    {
+        $this->mappingService->addException($mapping, $throwable);
+    }
+}
