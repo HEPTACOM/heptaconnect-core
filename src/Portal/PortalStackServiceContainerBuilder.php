3f8ff93e280d3587b1a45b76c3e9d2f664f7c6f7
Joshua Behrens <behrens@heptacom.de>
2021-01-18 19:49:39 +0100

    Ensure portal and extensions to be in the stack and getServices methods less error prone

--- src/Portal/PortalStackServiceContainerBuilder.php
+++ src/Portal/PortalStackServiceContainerBuilder.php
@@ -5,16 +5,23 @@ namespace Heptacom\HeptaConnect\Core\Portal;
 use Heptacom\HeptaConnect\Portal\Base\Portal\Contract\PortalContract;
 use Heptacom\HeptaConnect\Portal\Base\Portal\Contract\PortalExtensionContract;
 use Heptacom\HeptaConnect\Portal\Base\Portal\PortalExtensionCollection;
+use Heptacom\HeptaConnect\Portal\Base\Support\Contract\DeepCloneContract;
 
 class PortalStackServiceContainerBuilder
 {
     public function build(PortalContract $portal, PortalExtensionCollection $portalExtensions): PortalStackServiceContainer
     {
-        $services = $portal->getServices();
+        $services = $portal->getServices() + [
+            'portal' => $portal,
+            \get_class($portal) => $portal,
+        ];
 
         /** @var PortalExtensionContract $portalExtension */
         foreach ($portalExtensions as $portalExtension) {
             $services = $portalExtension->extendServices($services);
+
+            $services['portal_extensions'][] = $portalExtension;
+            $services[\get_class($portalExtension)] = $portalExtension;
         }
 
         return new PortalStackServiceContainer($services);
