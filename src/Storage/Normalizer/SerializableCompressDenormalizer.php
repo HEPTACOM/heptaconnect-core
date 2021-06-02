cfa400cc3871f75e41cf81e553a8d6020eb96ecf
Julian Krzefski <krzefski@heptacom.de>
2021-06-02 22:32:56 +0200

    Add compression to serialized php in portal storage

---
+++ src/Storage/Normalizer/SerializableCompressDenormalizer.php
@@ -0,0 +1,41 @@
+<?php
+declare(strict_types=1);
+
+namespace Heptacom\HeptaConnect\Core\Storage\Normalizer;
+
+use Heptacom\HeptaConnect\Core\Storage\Contract\DenormalizerInterface;
+
+class SerializableCompressDenormalizer implements DenormalizerInterface
+{
+    private DenormalizerInterface $serializableDenormalizer;
+
+    public function __construct(DenormalizerInterface $serializableDenormalizer)
+    {
+        $this->serializableDenormalizer = $serializableDenormalizer;
+    }
+
+    public function getType(): string
+    {
+        return $this->serializableDenormalizer->getType() . '+gzpress';
+    }
+
+    public function denormalize($data, $type, $format = null, array $context = [])
+    {
+        return $this->serializableDenormalizer->denormalize(
+            \gzuncompress($data),
+            $this->serializableDenormalizer->getType(),
+            $format,
+            $context
+        );
+    }
+
+    public function supportsDenormalization($data, $type, $format = null)
+    {
+        return $type === $this->getType() &&
+            $this->serializableDenormalizer->supportsDenormalization(
+                \gzuncompress($data),
+                $this->serializableDenormalizer->getType(),
+                $format
+            );
+    }
+}
---
+++ src/Storage/Normalizer/SerializableCompressNormalizer.php
@@ -0,0 +1,17 @@
+<?php
+declare(strict_types=1);
+
+namespace Heptacom\HeptaConnect\Core\Storage\Normalizer;
+
+class SerializableCompressNormalizer extends SerializableNormalizer
+{
+    public function getType(): string
+    {
+        return parent::getType() . '+gzpress';
+    }
+
+    public function normalize($object, $format = null, array $context = [])
+    {
+        return \gzcompress(parent::normalize($object, $format, $context));
+    }
+}
