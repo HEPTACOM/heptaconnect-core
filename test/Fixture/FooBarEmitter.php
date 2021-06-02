8eb372a99b73f0130d67f6b175c927495d43c005
Julian Krzefski <krzefski@heptacom.de>
2021-06-02 14:00:53 +0200

    Update tests to conform to new method signatures

--- test/Fixture/FooBarEmitter.php
+++ test/Fixture/FooBarEmitter.php
@@ -6,39 +6,23 @@ namespace Heptacom\HeptaConnect\Core\Test\Fixture;
 use Heptacom\HeptaConnect\Portal\Base\Emission\Contract\EmitContextInterface;
 use Heptacom\HeptaConnect\Portal\Base\Emission\Contract\EmitterContract;
 use Heptacom\HeptaConnect\Portal\Base\Emission\Contract\EmitterStackInterface;
-use Heptacom\HeptaConnect\Portal\Base\Mapping\MappedDatasetEntityStruct;
-use Heptacom\HeptaConnect\Portal\Base\Mapping\MappingCollection;
-use Heptacom\HeptaConnect\Portal\Base\StorageKey\Contract\MappingNodeKeyInterface;
-use Heptacom\HeptaConnect\Portal\Base\StorageKey\Contract\PortalNodeKeyInterface;
 
 class FooBarEmitter extends EmitterContract
 {
     private int $count;
 
-    private PortalNodeKeyInterface $portalNodeKey;
-
-    private MappingNodeKeyInterface $mappingNodeKey;
-
-    public function __construct(
-        int $count,
-        PortalNodeKeyInterface $portalNodeKey,
-        MappingNodeKeyInterface $mappingNodeKey
-    ) {
+    public function __construct(int $count)
+    {
         $this->count = $count;
-        $this->portalNodeKey = $portalNodeKey;
-        $this->mappingNodeKey = $mappingNodeKey;
     }
 
-    public function emit(MappingCollection $mappings, EmitContextInterface $context, EmitterStackInterface $stack): iterable
+    public function emit(iterable $externalIds, EmitContextInterface $context, EmitterStackInterface $stack): iterable
     {
         for ($c = 0; $c < $this->count; ++$c) {
-            yield new MappedDatasetEntityStruct(
-                new MappingStruct($this->portalNodeKey, $this->mappingNodeKey),
-                new FooBarEntity()
-            );
+            yield new FooBarEntity();
         }
 
-        yield from $stack->next($mappings, $context);
+        yield from $stack->next($externalIds, $context);
     }
 
     public function supports(): string
