0c5a82bde73ff537e2667ca59ac2c478c72c94ca
Joshua Behrens <behrens@heptacom.de>
2021-07-02 13:02:11 +0200

    Batch job repositories
    CONNECT-162

--- src/Job/JobDispatcher.php
+++ src/Job/JobDispatcher.php
@@ -4,9 +4,11 @@ declare(strict_types=1);
 namespace Heptacom\HeptaConnect\Core\Job;
 
 use Heptacom\HeptaConnect\Core\Flow\MessageQueueFlow\Message\JobMessage;
+use Heptacom\HeptaConnect\Core\Job\Contract\JobContract;
 use Heptacom\HeptaConnect\Core\Job\Contract\JobDispatcherContract;
 use Heptacom\HeptaConnect\Storage\Base\Contract\Repository\JobPayloadRepositoryContract;
 use Heptacom\HeptaConnect\Storage\Base\Contract\Repository\JobRepositoryContract;
+use Heptacom\HeptaConnect\Storage\Base\Repository\JobAdd;
 use Symfony\Component\Messenger\MessageBusInterface;
 
 class JobDispatcher extends JobDispatcherContract
@@ -29,13 +31,25 @@ class JobDispatcher extends JobDispatcherContract
 
     public function dispatch(JobCollection $jobs): void
     {
-        $jobKeys = [];
+        $payloads = [];
 
-        foreach ($jobs as $job) {
+        foreach ($jobs as $jobKey => $job) {
             $payload = $job->getPayload();
-            $payloadId = $payload === null ? null : $this->jobPayloadRepository->add($payload);
-            $jobKeys[] = $this->jobRepository->add($job->getMappingComponent(), $job->getType(), $payloadId);
+
+            if (\is_array($payload)) {
+                $payloads[$jobKey] = $payload;
+            }
         }
+
+        $payloadKeys = $this->jobPayloadRepository->add($payloads);
+        $jobAdds = [];
+
+        /** @var JobContract $job */
+        foreach ($jobs as $jobKey => $job) {
+            $jobAdds[$jobKey] = new JobAdd($job->getType(), $job->getMappingComponent(), $payloadKeys[$jobKey] ?? null);
+        }
+
+        $jobKeys = \array_values($this->jobRepository->add($jobAdds));
 
         if ($jobKeys === []) {
             return;
