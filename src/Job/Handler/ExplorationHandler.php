b1218ec5480b9f9bf3829881f797e720192a52c4
Joshua Behrens <behrens@heptacom.de>
2021-07-26 19:50:09 +0200

    Batch job based exploration better
    CONNECT-119

--- src/Job/Handler/ExplorationHandler.php
+++ src/Job/Handler/ExplorationHandler.php
@@ -4,26 +4,45 @@ declare(strict_types=1);
 namespace Heptacom\HeptaConnect\Core\Job\Handler;
 
 use Heptacom\HeptaConnect\Core\Exploration\Contract\ExploreServiceInterface;
-use Heptacom\HeptaConnect\Core\Job\JobData;
 use Heptacom\HeptaConnect\Core\Job\JobDataCollection;
+use Heptacom\HeptaConnect\Portal\Base\Mapping\Contract\MappingComponentStructContract;
+use Heptacom\HeptaConnect\Storage\Base\Contract\StorageKeyGeneratorContract;
 
 class ExplorationHandler
 {
     private ExploreServiceInterface $exploreService;
 
-    public function __construct(ExploreServiceInterface $exploreService)
-    {
+    private StorageKeyGeneratorContract $storageKeyGenerator;
+
+    public function __construct(
+        ExploreServiceInterface $exploreService,
+        StorageKeyGeneratorContract $storageKeyGenerator
+    ) {
         $this->exploreService = $exploreService;
+        $this->storageKeyGenerator = $storageKeyGenerator;
     }
 
-    public function triggerExploration(JobDataCollection $jobs): void
+    public function triggerExplorations(JobDataCollection $jobs): void
     {
-        /** @var JobData $job */
-        foreach ($jobs as $job) {
-            $this->exploreService->explore(
-                $job->getMappingComponent()->getPortalNodeKey(),
-                [$job->getMappingComponent()->getDatasetEntityClassName()]
-            );
+        $keys = [];
+        $types = [];
+
+        /** @var MappingComponentStructContract $mapping */
+        foreach ($jobs->column('getMappingComponent') as $mapping) {
+            $key = $this->storageKeyGenerator->serialize($mapping->getPortalNodeKey());
+
+            $keys[$key] = $mapping->getPortalNodeKey();
+            $types[$key][] = $mapping->getDatasetEntityClassName();
+        }
+
+        foreach ($keys as $key => $portalNodeKey) {
+            $type = $types[$key] ?? [];
+
+            if ($type === []) {
+                continue;
+            }
+
+            $this->exploreService->explore($portalNodeKey, $type);
         }
     }
 }
