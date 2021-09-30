9ffeb4f448541118293f17d3ea90e30fa3096a70
Michael Benz <benz@heptacom.de>
2021-09-30 15:49:44 +0200

    Add ExceptionCodeLogger and changelog entry

---
+++ Web/Http/Contract/HttpHandlerUrlProviderFactoryInterface.php
@@ -0,0 +1,13 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Heptacom\HeptaConnect\Core\Web\Http\Contract;
+
+use Heptacom\HeptaConnect\Portal\Base\StorageKey\Contract\PortalNodeKeyInterface;
+use Heptacom\HeptaConnect\Portal\Base\Web\Http\HttpHandlerUrlProviderInterface;
+
+interface HttpHandlerUrlProviderFactoryInterface
+{
+    public function factory(PortalNodeKeyInterface $portalNodeKey): HttpHandlerUrlProviderInterface;
+}
