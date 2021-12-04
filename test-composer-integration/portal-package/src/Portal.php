fd135c290955e9eff6020464912d1fa22a427382
Joshua Behrens <behrens@heptacom.de>
2021-12-04 17:33:24 +0100

    Move tests into framework

--- test-composer-integration/portal-package/src/Portal.php
+++
@@ -1,9 +0,0 @@
-<?php declare(strict_types=1);
-
-namespace HeptacomFixture\Portal\A;
-
-use Heptacom\HeptaConnect\Portal\Base\Portal\Contract\PortalContract;
-
-class Portal extends PortalContract
-{
-}
