08835204d84db3b602360377e2bd383cebc6fda8
Joshua Behrens <behrens@heptacom.de>
2021-12-18 15:18:09 +0100

    Merge remote-tracking branch 'origin/feature/CONNECT-291' into feature/CONNECT-291

---
+++ Component/Composer/PackageConfigurationClassMap.php
@@ -0,0 +1,66 @@
+<?php
+declare(strict_types=1);
+
+namespace Heptacom\HeptaConnect\Core\Component\Composer;
+
+use Heptacom\HeptaConnect\Dataset\Base\ScalarCollection\StringCollection;
+
+class PackageConfigurationClassMap
+{
+    /**
+     * @psalm-var array<class-string, string>
+     *
+     * @var array|string[]
+     */
+    private array $map = [];
+
+    /**
+     * @psalm-param class-string $fqcn
+     */
+    public function addClass(string $fqcn, string $filePath): void
+    {
+        $this->map[$fqcn] = $filePath;
+    }
+
+    public function count(): int
+    {
+        return \count($this->map);
+    }
+
+    public function clear(): void
+    {
+        $this->map = [];
+    }
+
+    /**
+     * @psalm-param class-string $fqcn
+     */
+    public function getFileForClass(string $fqcn): ?string
+    {
+        return $this->map[$fqcn] ?? null;
+    }
+
+    /**
+     * @psalm-return class-string|null
+     */
+    public function getClassForFile(string $filePath): ?string
+    {
+        $index = \array_search($filePath, $this->map, true);
+
+        if ($index === false) {
+            return null;
+        }
+
+        return $index;
+    }
+
+    public function getClasses(): StringCollection
+    {
+        return new StringCollection(\array_keys($this->map));
+    }
+
+    public function getFiles(): StringCollection
+    {
+        return new StringCollection(\array_values($this->map));
+    }
+}
---
+++ Flow/MessageQueueFlow/Message/JobMessage.php
@@ -0,0 +1,21 @@
+<?php
+declare(strict_types=1);
+
+namespace Heptacom\HeptaConnect\Core\Flow\MessageQueueFlow\Message;
+
+use Heptacom\HeptaConnect\Storage\Base\JobKeyCollection;
+
+class JobMessage
+{
+    private JobKeyCollection $jobKeys;
+
+    public function __construct()
+    {
+        $this->jobKeys = new JobKeyCollection();
+    }
+
+    public function getJobKeys(): JobKeyCollection
+    {
+        return $this->jobKeys;
+    }
+}
