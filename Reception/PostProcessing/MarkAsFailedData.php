575ad881694dbef9c97d69dbb2821b4f21b1d87d
Julian Krzefski <krzefski@heptacom.de>
2021-12-18 14:50:42 +0100

    CONNECT-291: Add JobFailAction

---
+++ Reception/PostProcessing/MarkAsFailedData.php
@@ -0,0 +1,29 @@
+<?php
+declare(strict_types=1);
+
+namespace Heptacom\HeptaConnect\Core\Reception\PostProcessing;
+
+use Heptacom\HeptaConnect\Dataset\Base\Contract\DatasetEntityContract;
+
+class MarkAsFailedData
+{
+    private DatasetEntityContract $entity;
+
+    private \Throwable $throwable;
+
+    public function __construct(DatasetEntityContract $entity, \Throwable $throwable)
+    {
+        $this->entity = $entity;
+        $this->throwable = $throwable;
+    }
+
+    public function getEntity(): DatasetEntityContract
+    {
+        return $this->entity;
+    }
+
+    public function getThrowable(): \Throwable
+    {
+        return $this->throwable;
+    }
+}
