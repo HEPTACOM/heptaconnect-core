a65c26f3f1ffb1ed89571ea4708b2db2f6d920e4
Joshua Behrens <behrens@heptacom.de>
2022-01-02 01:15:49 +0100

    Create service to for code origin finding for status reporters
    CONNECT-284

---
+++ StatusReporting/StatusReporterCodeOriginFinder.php
@@ -0,0 +1,59 @@
+<?php
+declare(strict_types=1);
+
+namespace Heptacom\HeptaConnect\Core\StatusReporting;
+
+use Closure;
+use Heptacom\HeptaConnect\Portal\Base\Builder\Component\StatusReporter;
+use Heptacom\HeptaConnect\Portal\Base\FlowComponent\CodeOrigin;
+use Heptacom\HeptaConnect\Portal\Base\FlowComponent\Exception\CodeOriginNotFound;
+use Heptacom\HeptaConnect\Portal\Base\StatusReporting\Contract\StatusReporterCodeOriginFinderInterface;
+use Heptacom\HeptaConnect\Portal\Base\StatusReporting\Contract\StatusReporterContract;
+use ReflectionClass;
+use ReflectionException;
+use ReflectionFunction;
+
+class StatusReporterCodeOriginFinder implements StatusReporterCodeOriginFinderInterface
+{
+    public function findOrigin(StatusReporterContract $statusReporter): CodeOrigin
+    {
+        if ($statusReporter instanceof StatusReporter) {
+            /** @var array<Closure|null> $closures */
+            $closures = [
+                $statusReporter->getRunMethod(),
+            ];
+
+            $lastReflectionException = null;
+
+            foreach ($closures as $closure) {
+                if ($closure instanceof Closure) {
+                    try {
+                        $reflection = new ReflectionFunction($closure);
+                        $filepath = $reflection->getFileName();
+
+                        if (\is_string($filepath)) {
+                            return new CodeOrigin($filepath, $reflection->getStartLine(), $reflection->getEndLine());
+                        }
+                    } catch (ReflectionException $e) {
+                        $lastReflectionException = $e;
+                    }
+                }
+            }
+
+            throw new CodeOriginNotFound($statusReporter, 1641079371, $lastReflectionException);
+        }
+
+        try {
+            $reflection = new ReflectionClass($statusReporter);
+            $filepath = $reflection->getFileName();
+
+            if (\is_string($filepath)) {
+                return new CodeOrigin($filepath, $reflection->getStartLine(), $reflection->getEndLine());
+            }
+        } catch (ReflectionException $e) {
+            throw new CodeOriginNotFound($statusReporter, 1641079372, $e);
+        }
+
+        throw new CodeOriginNotFound($statusReporter, 1641079373);
+    }
+}
