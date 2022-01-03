544a08c75d3909023c9ffea5a9eae07da17c88be
Joshua Behrens <behrens@heptacom.de>
2022-01-03 11:07:19 +0100

    Move action interfaces

--- Flow/MessageQueueFlow/MessageHandler.php
+++ Flow/MessageQueueFlow/MessageHandler.php
@@ -9,7 +9,7 @@ use Heptacom\HeptaConnect\Core\Job\Contract\DelegatingJobActorContract;
 use Heptacom\HeptaConnect\Core\Job\JobData;
 use Heptacom\HeptaConnect\Core\Job\JobDataCollection;
 use Heptacom\HeptaConnect\Storage\Base\Action\Job\Get\JobGetCriteria;
-use Heptacom\HeptaConnect\Storage\Base\Contract\Action\Job\Get\JobGetActionInterface;
+use Heptacom\HeptaConnect\Storage\Base\Contract\Action\Job\JobGetActionInterface;
 use Symfony\Component\Messenger\Handler\MessageSubscriberInterface;
 
 class MessageHandler implements MessageSubscriberInterface
