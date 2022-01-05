05023fb2f0710e87f295e5678135c0f8580f5c67
Joshua Behrens <behrens@heptacom.de>
2022-01-06 00:01:34 +0100

    Merge remote-tracking branch 'core/feature/CONNECT-72' into feature/CONNECT-72

---
+++ Job/JobDispatcher.php
@@ -0,0 +1,42 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Heptacom\HeptaConnect\Core\Job;
+
+use Heptacom\HeptaConnect\Core\Flow\MessageQueueFlow\Message\JobMessage;
+use Heptacom\HeptaConnect\Core\Job\Contract\JobContract;
+use Heptacom\HeptaConnect\Core\Job\Contract\JobDispatcherContract;
+use Heptacom\HeptaConnect\Storage\Base\Contract\Action\Job\Create\JobCreateActionInterface;
+use Heptacom\HeptaConnect\Storage\Base\Contract\Action\Job\Create\JobCreatePayload;
+use Heptacom\HeptaConnect\Storage\Base\Contract\Action\Job\Create\JobCreatePayloads;
+use Symfony\Component\Messenger\MessageBusInterface;
+
+class JobDispatcher extends JobDispatcherContract
+{
+    private MessageBusInterface $bus;
+
+    private JobCreateActionInterface $jobCreateAction;
+
+    public function __construct(MessageBusInterface $bus, JobCreateActionInterface $jobCreateAction)
+    {
+        $this->bus = $bus;
+        $this->jobCreateAction = $jobCreateAction;
+    }
+
+    public function dispatch(JobCollection $jobs): void
+    {
+        $createPayload = new JobCreatePayloads($jobs->map(
+            static fn (JobContract $j): JobCreatePayload => new JobCreatePayload($j->getType(), $j->getMappingComponent(), $j->getPayload())
+        ));
+        $createResult = $this->jobCreateAction->create($createPayload);
+
+        if ($createResult->count() === 0) {
+            return;
+        }
+
+        $message = new JobMessage();
+        $message->getJobKeys()->push($createResult->column('getJobKey'));
+        $this->bus->dispatch($message);
+    }
+}
---
+++ Parallelization/ResourceLocking.php
@@ -0,0 +1,75 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Heptacom\HeptaConnect\Core\Parallelization;
+
+use Heptacom\HeptaConnect\Portal\Base\Parallelization\Contract\ResourceLockingContract;
+use Heptacom\HeptaConnect\Portal\Base\Parallelization\Exception\ResourceIsLockedException;
+use Heptacom\HeptaConnect\Portal\Base\StorageKey\Contract\StorageKeyInterface;
+use Heptacom\HeptaConnect\Storage\Base\Contract\ResourceLockStorageContract;
+use Heptacom\HeptaConnect\Storage\Base\Contract\StorageKeyGeneratorContract;
+use Heptacom\HeptaConnect\Storage\Base\Exception\UnsupportedStorageKeyException;
+
+/**
+ * Resource locking is currently not working properly
+ * as locks are released prematurely. This depends on
+ * the underlying storage implementation. Currently
+ * it is unclear whether a fix will require breaking
+ * changes to the public api.
+ *
+ * @internal
+ */
+class ResourceLocking extends ResourceLockingContract
+{
+    private ResourceLockStorageContract $resourceLockStorage;
+
+    private StorageKeyGeneratorContract $storageKeyGenerator;
+
+    public function __construct(
+        ResourceLockStorageContract $resourceLockStorage,
+        StorageKeyGeneratorContract $storageKeyGenerator
+    ) {
+        $this->resourceLockStorage = $resourceLockStorage;
+        $this->storageKeyGenerator = $storageKeyGenerator;
+    }
+
+    public function isLocked(string $resourceKey, ?StorageKeyInterface $owner): bool
+    {
+        return $this->resourceLockStorage->has($this->buildKey($resourceKey, $owner));
+    }
+
+    public function lock(string $resourceKey, ?StorageKeyInterface $owner): void
+    {
+        $key = $this->buildKey($resourceKey, $owner);
+
+        if ($this->resourceLockStorage->has($key)) {
+            throw new ResourceIsLockedException($key, $owner);
+        }
+
+        $this->resourceLockStorage->create($key);
+    }
+
+    public function release(string $resourceKey, ?StorageKeyInterface $owner): void
+    {
+        $key = $this->buildKey($resourceKey, $owner);
+
+        if ($this->resourceLockStorage->has($key)) {
+            $this->resourceLockStorage->delete($key);
+        }
+    }
+
+    private function buildKey(string $resourceKey, ?StorageKeyInterface $owner): string
+    {
+        $prefix = 'ownerless';
+
+        if ($owner instanceof StorageKeyInterface) {
+            try {
+                $prefix = $this->storageKeyGenerator->serialize($owner);
+            } catch (UnsupportedStorageKeyException $e) {
+            }
+        }
+
+        return $prefix . $resourceKey;
+    }
+}
