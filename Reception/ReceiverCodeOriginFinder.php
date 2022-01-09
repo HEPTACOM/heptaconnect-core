da99c93d3da2e92037f55f91664b16213810cf88
Joshua Behrens <behrens@heptacom.de>
2022-01-09 01:42:07 +0100

    $ make cs-fix-php

--- Reception/ReceiverCodeOriginFinder.php
+++ Reception/ReceiverCodeOriginFinder.php
@@ -1,24 +1,21 @@
 <?php
+
 declare(strict_types=1);
 
 namespace Heptacom\HeptaConnect\Core\Reception;
 
-use Closure;
 use Heptacom\HeptaConnect\Portal\Base\Builder\Component\Receiver;
 use Heptacom\HeptaConnect\Portal\Base\FlowComponent\CodeOrigin;
 use Heptacom\HeptaConnect\Portal\Base\FlowComponent\Exception\CodeOriginNotFound;
 use Heptacom\HeptaConnect\Portal\Base\Reception\Contract\ReceiverCodeOriginFinderInterface;
 use Heptacom\HeptaConnect\Portal\Base\Reception\Contract\ReceiverContract;
-use ReflectionClass;
-use ReflectionException;
-use ReflectionFunction;
 
 class ReceiverCodeOriginFinder implements ReceiverCodeOriginFinderInterface
 {
     public function findOrigin(ReceiverContract $receiver): CodeOrigin
     {
         if ($receiver instanceof Receiver) {
-            /** @var array<Closure|null> $closures */
+            /** @var array<\Closure|null> $closures */
             $closures = [
                 $receiver->getRunMethod(),
                 $receiver->getBatchMethod(),
@@ -27,15 +24,15 @@ class ReceiverCodeOriginFinder implements ReceiverCodeOriginFinderInterface
             $lastReflectionException = null;
 
             foreach ($closures as $closure) {
-                if ($closure instanceof Closure) {
+                if ($closure instanceof \Closure) {
                     try {
-                        $reflection = new ReflectionFunction($closure);
+                        $reflection = new \ReflectionFunction($closure);
                         $filepath = $reflection->getFileName();
 
                         if (\is_string($filepath)) {
                             return new CodeOrigin($filepath, $reflection->getStartLine(), $reflection->getEndLine());
                         }
-                    } catch (ReflectionException $e) {
+                    } catch (\ReflectionException $e) {
                         $lastReflectionException = $e;
                     }
                 }
@@ -45,13 +42,13 @@ class ReceiverCodeOriginFinder implements ReceiverCodeOriginFinderInterface
         }
 
         try {
-            $reflection = new ReflectionClass($receiver);
+            $reflection = new \ReflectionClass($receiver);
             $filepath = $reflection->getFileName();
 
             if (\is_string($filepath)) {
                 return new CodeOrigin($filepath, $reflection->getStartLine(), $reflection->getEndLine());
             }
-        } catch (ReflectionException $e) {
+        } catch (\ReflectionException $e) {
             throw new CodeOriginNotFound($receiver, 1641079369, $e);
         }
 
--- Web/Http/HttpHandlerCodeOriginFinder.php
+++ Web/Http/HttpHandlerCodeOriginFinder.php
@@ -1,24 +1,21 @@
 <?php
+
 declare(strict_types=1);
 
 namespace Heptacom\HeptaConnect\Core\Web\Http;
 
-use Closure;
 use Heptacom\HeptaConnect\Portal\Base\Builder\Component\HttpHandler;
 use Heptacom\HeptaConnect\Portal\Base\FlowComponent\CodeOrigin;
 use Heptacom\HeptaConnect\Portal\Base\FlowComponent\Exception\CodeOriginNotFound;
 use Heptacom\HeptaConnect\Portal\Base\Web\Http\Contract\HttpHandlerCodeOriginFinderInterface;
 use Heptacom\HeptaConnect\Portal\Base\Web\Http\Contract\HttpHandlerContract;
-use ReflectionClass;
-use ReflectionException;
-use ReflectionFunction;
 
 class HttpHandlerCodeOriginFinder implements HttpHandlerCodeOriginFinderInterface
 {
     public function findOrigin(HttpHandlerContract $httpHandler): CodeOrigin
     {
         if ($httpHandler instanceof HttpHandler) {
-            /** @var array<Closure|null> $closures */
+            /** @var array<\Closure|null> $closures */
             $closures = [
                 $httpHandler->getRunMethod(),
                 $httpHandler->getOptionsMethod(),
@@ -32,15 +29,15 @@ class HttpHandlerCodeOriginFinder implements HttpHandlerCodeOriginFinderInterfac
             $lastReflectionException = null;
 
             foreach ($closures as $closure) {
-                if ($closure instanceof Closure) {
+                if ($closure instanceof \Closure) {
                     try {
-                        $reflection = new ReflectionFunction($closure);
+                        $reflection = new \ReflectionFunction($closure);
                         $filepath = $reflection->getFileName();
 
                         if (\is_string($filepath)) {
                             return new CodeOrigin($filepath, $reflection->getStartLine(), $reflection->getEndLine());
                         }
-                    } catch (ReflectionException $e) {
+                    } catch (\ReflectionException $e) {
                         $lastReflectionException = $e;
                     }
                 }
@@ -50,13 +47,13 @@ class HttpHandlerCodeOriginFinder implements HttpHandlerCodeOriginFinderInterfac
         }
 
         try {
-            $reflection = new ReflectionClass($httpHandler);
+            $reflection = new \ReflectionClass($httpHandler);
             $filepath = $reflection->getFileName();
 
             if (\is_string($filepath)) {
                 return new CodeOrigin($filepath, $reflection->getStartLine(), $reflection->getEndLine());
             }
-        } catch (ReflectionException $e) {
+        } catch (\ReflectionException $e) {
             throw new CodeOriginNotFound($httpHandler, 1637607700, $e);
         }
 
