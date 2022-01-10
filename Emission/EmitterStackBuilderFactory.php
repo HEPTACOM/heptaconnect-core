a848cb1e849abb1c5455bb2c86b12ca2d407d3e8
Julian Krzefski <krzefski@heptacom.de>
2022-01-10 12:18:37 +0000

    Merged in feature/make-alias-public (pull request #11)
    Make all aliases in portal container public
    
    Approved-by: Joshua Behrens

---
+++ Emission/EmitterStackBuilderFactory.php
@@ -0,0 +1,42 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Heptacom\HeptaConnect\Core\Emission;
+
+use Heptacom\HeptaConnect\Core\Emission\Contract\EmitterStackBuilderFactoryInterface;
+use Heptacom\HeptaConnect\Core\Emission\Contract\EmitterStackBuilderInterface;
+use Heptacom\HeptaConnect\Core\Portal\FlowComponentRegistry;
+use Heptacom\HeptaConnect\Core\Portal\PortalStackServiceContainerFactory;
+use Heptacom\HeptaConnect\Portal\Base\Emission\EmitterCollection;
+use Heptacom\HeptaConnect\Portal\Base\StorageKey\Contract\PortalNodeKeyInterface;
+use Psr\Log\LoggerInterface;
+
+class EmitterStackBuilderFactory implements EmitterStackBuilderFactoryInterface
+{
+    private PortalStackServiceContainerFactory $portalContainerFactory;
+
+    private LoggerInterface $logger;
+
+    public function __construct(PortalStackServiceContainerFactory $portalContainerFactory, LoggerInterface $logger)
+    {
+        $this->portalContainerFactory = $portalContainerFactory;
+        $this->logger = $logger;
+    }
+
+    public function createEmitterStackBuilder(
+        PortalNodeKeyInterface $portalNodeKey,
+        string $entityType
+    ): EmitterStackBuilderInterface {
+        $container = $this->portalContainerFactory->create($portalNodeKey);
+        /** @var FlowComponentRegistry $flowComponentRegistry */
+        $flowComponentRegistry = $container->get(FlowComponentRegistry::class);
+        $components = new EmitterCollection();
+
+        foreach ($flowComponentRegistry->getOrderedSources() as $source) {
+            $components->push($flowComponentRegistry->getEmitters($source));
+        }
+
+        return new EmitterStackBuilder($components, $entityType, $this->logger);
+    }
+}
