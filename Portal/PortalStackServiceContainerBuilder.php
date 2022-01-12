61645e0b5f22363dbaad82db1a78b9a6dc87eebc
Joshua Behrens <behrens@heptacom.de>
2022-01-12 21:48:26 +0100

    Move exception
    CONNECT-200

--- Portal/PortalStackServiceContainerBuilder.php
+++ Portal/PortalStackServiceContainerBuilder.php
@@ -223,7 +223,7 @@ class PortalStackServiceContainerBuilder implements PortalStackServiceContainerB
                     new Reference(ClientInterface::class),
                     new Reference(UriFactoryInterface::class),
                 ])
-                ->addMethodCall('withExceptionTriggers', [...\range(400, 599)], true)
+                ->addMethodCall('withExceptionTriggers', \range(400, 599), true)
                 ->addMethodCall('withMaxRedirect', [20], true)
                 ->addMethodCall('withMaxRetry', [2], true)
                 ->addMethodCall('withMaxWaitTimeout', [], true)
