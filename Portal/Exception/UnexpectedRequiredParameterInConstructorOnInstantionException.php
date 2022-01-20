edce99ce18fcd6a176fb93d8905bd6821825e418
Michael Benz <benz@heptacom.de>
2022-01-20 18:20:55 +0100

    Consider throwable instead of only exception, position independent

---
+++ Portal/Exception/UnexpectedRequiredParameterInConstructorOnInstantionException.php
@@ -0,0 +1,9 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Heptacom\HeptaConnect\Core\Portal\Exception;
+
+class UnexpectedRequiredParameterInConstructorOnInstantionException extends AbstractInstantiationException
+{
+}
