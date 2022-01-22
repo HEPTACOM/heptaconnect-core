9ce3fad090ebadd7ed4edfd3b3d3fe27d6bedf8a
Joshua Behrens <behrens@heptacom.de>
2022-01-22 18:22:27 +0100

    $ make cs-fix-composer
    CONNECT-126

---
+++ Configuration/ConfigurationService.php
@@ -0,0 +1,145 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Heptacom\HeptaConnect\Core\Configuration;
+
+use Heptacom\HeptaConnect\Core\Configuration\Contract\ConfigurationServiceInterface;
+use Heptacom\HeptaConnect\Core\Portal\Contract\PortalRegistryInterface;
+use Heptacom\HeptaConnect\Portal\Base\StorageKey\Contract\PortalNodeKeyInterface;
+use Heptacom\HeptaConnect\Portal\Base\StorageKey\PortalNodeKeyCollection;
+use Heptacom\HeptaConnect\Storage\Base\Action\PortalNodeConfiguration\Get\PortalNodeConfigurationGetCriteria;
+use Heptacom\HeptaConnect\Storage\Base\Action\PortalNodeConfiguration\Get\PortalNodeConfigurationGetResult;
+use Heptacom\HeptaConnect\Storage\Base\Action\PortalNodeConfiguration\Set\PortalNodeConfigurationSetPayload;
+use Heptacom\HeptaConnect\Storage\Base\Action\PortalNodeConfiguration\Set\PortalNodeConfigurationSetPayloads;
+use Heptacom\HeptaConnect\Storage\Base\Contract\Action\PortalNodeConfiguration\PortalNodeConfigurationGetActionInterface;
+use Heptacom\HeptaConnect\Storage\Base\Contract\Action\PortalNodeConfiguration\PortalNodeConfigurationSetActionInterface;
+use Heptacom\HeptaConnect\Storage\Base\Contract\StorageKeyGeneratorContract;
+use Psr\Cache\CacheItemPoolInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class ConfigurationService implements ConfigurationServiceInterface
+{
+    private PortalRegistryInterface $portalRegistry;
+
+    private CacheItemPoolInterface $cache;
+
+    private StorageKeyGeneratorContract $keyGenerator;
+
+    private PortalNodeConfigurationGetActionInterface $portalNodeConfigurationGet;
+
+    private PortalNodeConfigurationSetActionInterface $portalNodeConfigurationSet;
+
+    public function __construct(
+        PortalRegistryInterface $portalRegistry,
+        CacheItemPoolInterface $cache,
+        StorageKeyGeneratorContract $keyGenerator,
+        PortalNodeConfigurationGetActionInterface $portalNodeConfigurationGet,
+        PortalNodeConfigurationSetActionInterface $portalNodeConfigurationSet
+    ) {
+        $this->portalRegistry = $portalRegistry;
+        $this->cache = $cache;
+        $this->keyGenerator = $keyGenerator;
+        $this->portalNodeConfigurationGet = $portalNodeConfigurationGet;
+        $this->portalNodeConfigurationSet = $portalNodeConfigurationSet;
+    }
+
+    public function getPortalNodeConfiguration(PortalNodeKeyInterface $portalNodeKey): ?array
+    {
+        $cachedConfig = $this->cache->getItem($this->getConfigCacheKey($portalNodeKey));
+
+        if (!$cachedConfig->isHit()) {
+            $template = $this->getMergedConfigurationTemplate($portalNodeKey);
+            $config = $template->resolve($this->getPortalNodeConfigurationInternal($portalNodeKey));
+
+            $this->cache->save($cachedConfig->set($config));
+        } else {
+            $config = $cachedConfig->get();
+        }
+
+        return $config;
+    }
+
+    public function setPortalNodeConfiguration(PortalNodeKeyInterface $portalNodeKey, ?array $configuration): void
+    {
+        $cachedConfigKey = $this->getConfigCacheKey($portalNodeKey);
+        $cachedConfig = $this->cache->getItem($cachedConfigKey);
+
+        if ($cachedConfig->isHit()) {
+            $this->cache->deleteItem($cachedConfigKey);
+        }
+
+        $template = $this->getMergedConfigurationTemplate($portalNodeKey);
+
+        if ($configuration === null) {
+            $data = null;
+        } else {
+            $data = $this->getPortalNodeConfigurationInternal($portalNodeKey);
+            $data = $this->removeStorageKeysWhenValueIsNull($data, $configuration ?? []);
+            $configuration = $this->removeStorageKeysWhenValueIsNull($configuration, $configuration ?? []);
+            $data = \array_replace_recursive($data, $configuration);
+
+            $template->resolve($data);
+        }
+
+        $this->portalNodeConfigurationSet->set(new PortalNodeConfigurationSetPayloads([
+            new PortalNodeConfigurationSetPayload($portalNodeKey, $data),
+        ]));
+    }
+
+    /**
+     * @TODO extract for easier testing
+     */
+    private function removeStorageKeysWhenValueIsNull(array $editable, array $nullArray): array
+    {
+        foreach ($nullArray as $key => $value) {
+            if (\is_array($value) && \array_key_exists($key, $editable)) {
+                $editable[$key] = $this->removeStorageKeysWhenValueIsNull($editable[$key], $value);
+
+                continue;
+            }
+
+            if ($value !== null) {
+                continue;
+            }
+
+            unset($editable[$key]);
+        }
+
+        return $editable;
+    }
+
+    private function getMergedConfigurationTemplate(PortalNodeKeyInterface $portalNodeKey): OptionsResolver
+    {
+        $portal = $this->portalRegistry->getPortal($portalNodeKey);
+
+        $template = $portal->getConfigurationTemplate();
+        $extensions = $this->portalRegistry->getPortalExtensions($portalNodeKey);
+
+        foreach ($extensions as $extension) {
+            $template = $extension->extendConfiguration($template);
+        }
+
+        return $template;
+    }
+
+    private function getConfigCacheKey(PortalNodeKeyInterface $portalNodeKey): string
+    {
+        $key = $this->keyGenerator->serialize($portalNodeKey);
+        $key = \str_replace(['{', '}', '(', ')', '/', '\\', '@', ':'], '', $key);
+
+        return 'config.cache.' . $key;
+    }
+
+    private function getPortalNodeConfigurationInternal(PortalNodeKeyInterface $portalNodeKey): array
+    {
+        $criteria = new PortalNodeConfigurationGetCriteria(new PortalNodeKeyCollection([$portalNodeKey]));
+
+        /** @var PortalNodeConfigurationGetResult $configuration */
+        foreach ($this->portalNodeConfigurationGet->get($criteria) as $configuration) {
+            return $configuration->getValue();
+        }
+
+        return [];
+    }
+}
---
+++ Reception/ReceiverStackBuilder.php
@@ -0,0 +1,117 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Heptacom\HeptaConnect\Core\Reception;
+
+use Heptacom\HeptaConnect\Core\Reception\Contract\ReceiverStackBuilderInterface;
+use Heptacom\HeptaConnect\Portal\Base\Reception\Contract\ReceiverContract;
+use Heptacom\HeptaConnect\Portal\Base\Reception\Contract\ReceiverStackInterface;
+use Heptacom\HeptaConnect\Portal\Base\Reception\ReceiverCollection;
+use Heptacom\HeptaConnect\Portal\Base\Reception\ReceiverStack;
+use Psr\Log\LoggerInterface;
+
+class ReceiverStackBuilder implements ReceiverStackBuilderInterface
+{
+    private ?ReceiverContract $source;
+
+    private ReceiverCollection $decorators;
+
+    private LoggerInterface $logger;
+
+    /**
+     * @var class-string<\Heptacom\HeptaConnect\Dataset\Base\Contract\DatasetEntityContract>
+     */
+    private string $entityType;
+
+    /**
+     * @var ReceiverContract[]
+     */
+    private array $receivers = [];
+
+    /**
+     * @param class-string<\Heptacom\HeptaConnect\Dataset\Base\Contract\DatasetEntityContract> $entityType
+     */
+    public function __construct(
+        ReceiverCollection $sources,
+        string $entityType,
+        LoggerInterface $logger
+    ) {
+        $sources = new ReceiverCollection($sources->bySupport($entityType));
+        $this->source = $sources->shift();
+        $this->decorators = $sources;
+        $this->entityType = $entityType;
+        $this->logger = $logger;
+    }
+
+    public function push(ReceiverContract $receiver): self
+    {
+        if (\is_a($this->entityType, $receiver->supports(), true)) {
+            $this->logger->debug('ReceiverStackBuilder: Pushed an arbitrary receiver.', [
+                'receiver' => $receiver,
+            ]);
+
+            $this->receivers[] = $receiver;
+        } else {
+            $this->logger->debug(
+                \sprintf(
+                    'ReceiverStackBuilder: Tried to push an arbitrary receiver, but it does not support type %s.',
+                    $this->entityType,
+                ),
+                [
+                    'receiver' => $receiver,
+                ]
+            );
+        }
+
+        return $this;
+    }
+
+    public function pushSource(): self
+    {
+        if ($this->source instanceof ReceiverContract) {
+            $this->logger->debug('ReceiverStackBuilder: Pushed the source receiver.', [
+                'receiver' => $this->source,
+            ]);
+
+            if (!\in_array($this->source, $this->receivers, true)) {
+                $this->receivers[] = $this->source;
+            }
+        }
+
+        return $this;
+    }
+
+    public function pushDecorators(): self
+    {
+        foreach ($this->decorators as $receiver) {
+            $this->logger->debug('ReceiverStackBuilder: Pushed a decorator receiver.', [
+                'receiver' => $receiver,
+            ]);
+
+            if (!\in_array($receiver, $this->receivers, true)) {
+                $this->receivers[] = $receiver;
+            }
+        }
+
+        return $this;
+    }
+
+    public function build(): ReceiverStackInterface
+    {
+        $receiverStack = new ReceiverStack(\array_map(
+            static fn (ReceiverContract $e) => clone $e,
+            \array_reverse($this->receivers, false),
+        ));
+        $receiverStack->setLogger($this->logger);
+
+        $this->logger->debug('ReceiverStackBuilder: Built receiver stack.');
+
+        return $receiverStack;
+    }
+
+    public function isEmpty(): bool
+    {
+        return $this->receivers === [];
+    }
+}
