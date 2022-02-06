29ece81cbd0f1f0294067498e692c44f13d0adaf
Joshua Behrens <behrens@heptacom.de>
2022-02-06 18:47:23 +0100

    Remove MappingService::merge
    CONNECT-126

--- Mapping/MappingService.php
+++ Mapping/MappingService.php
@@ -5,41 +5,26 @@ declare(strict_types=1);
 namespace Heptacom\HeptaConnect\Core\Mapping;
 
 use Heptacom\HeptaConnect\Core\Mapping\Contract\MappingServiceInterface;
-use Heptacom\HeptaConnect\Core\Mapping\Exception\MappingNodeAreUnmergableException;
-use Heptacom\HeptaConnect\Portal\Base\Mapping\Contract\MappingInterface;
-use Heptacom\HeptaConnect\Portal\Base\StorageKey\Contract\MappingKeyInterface;
 use Heptacom\HeptaConnect\Portal\Base\StorageKey\Contract\MappingNodeKeyInterface;
 use Heptacom\HeptaConnect\Portal\Base\StorageKey\Contract\PortalNodeKeyInterface;
 use Heptacom\HeptaConnect\Storage\Base\Contract\Repository\MappingExceptionRepositoryContract;
-use Heptacom\HeptaConnect\Storage\Base\Contract\Repository\MappingNodeRepositoryContract;
-use Heptacom\HeptaConnect\Storage\Base\Contract\Repository\MappingRepositoryContract;
 use Heptacom\HeptaConnect\Storage\Base\Contract\StorageKeyGeneratorContract;
-use Heptacom\HeptaConnect\Storage\Base\Exception\NotFoundException;
-use Heptacom\HeptaConnect\Storage\Base\Exception\UnsupportedStorageKeyException;
 use Psr\Log\LoggerInterface;
 
 class MappingService implements MappingServiceInterface
 {
-    private MappingRepositoryContract $mappingRepository;
-
     private MappingExceptionRepositoryContract $mappingExceptionRepository;
-
-    private MappingNodeRepositoryContract $mappingNodeRepository;
 
     private StorageKeyGeneratorContract $storageKeyGenerator;
 
     private LoggerInterface $logger;
 
     public function __construct(
-        MappingRepositoryContract $mappingRepository,
         MappingExceptionRepositoryContract $mappingExceptionRepository,
-        MappingNodeRepositoryContract $mappingNodeRepository,
         StorageKeyGeneratorContract $storageKeyGenerator,
         LoggerInterface $logger
     ) {
-        $this->mappingRepository = $mappingRepository;
         $this->mappingExceptionRepository = $mappingExceptionRepository;
-        $this->mappingNodeRepository = $mappingNodeRepository;
         $this->storageKeyGenerator = $storageKeyGenerator;
         $this->logger = $logger;
     }
@@ -58,58 +43,6 @@ class MappingService implements MappingServiceInterface
                 'portalNodeKey' => $this->storageKeyGenerator->serialize($portalNodeKey),
                 'outerException' => $throwable,
             ]);
-        }
-    }
-
-    public function merge(MappingNodeKeyInterface $mergeFrom, MappingNodeKeyInterface $mergeInto): void
-    {
-        try {
-            $nodeFrom = $this->mappingNodeRepository->read($mergeFrom);
-            $nodeInto = $this->mappingNodeRepository->read($mergeInto);
-            if ($nodeFrom->getEntityType() !== $nodeInto->getEntityType()) {
-                throw new MappingNodeAreUnmergableException($mergeFrom, $mergeInto);
-            }
-
-            $intoPortalExistences = [];
-
-            foreach ($this->mappingRepository->listByMappingNode($mergeInto) as $mappingKey) {
-                $mapping = $this->mappingRepository->read($mappingKey);
-                $portalNode = $this->storageKeyGenerator->serialize($mapping->getPortalNodeKey());
-
-                $intoPortalExistences[$portalNode] = $mapping->getExternalId();
-            }
-
-            /** @var MappingInterface[] $mappingsToCreate */
-            $mappingsToCreate = [];
-            /** @var MappingKeyInterface[] $mappingsToDelete */
-            $mappingsToDelete = [];
-
-            foreach ($this->mappingRepository->listByMappingNode($mergeFrom) as $mappingKey) {
-                $mapping = $this->mappingRepository->read($mappingKey);
-                $portalNode = $this->storageKeyGenerator->serialize($mapping->getPortalNodeKey());
-
-                if (\array_key_exists($portalNode, $intoPortalExistences) && $intoPortalExistences[$portalNode] !== null) {
-                    if ($intoPortalExistences[$portalNode] !== $mapping->getExternalId()) {
-                        throw new MappingNodeAreUnmergableException($mergeFrom, $mergeInto);
-                    }
-                } else {
-                    $mappingsToCreate[] = $mapping;
-                }
-
-                $mappingsToDelete[] = $mappingKey;
-            }
-
-            \array_walk($mappingsToDelete, [$this->mappingRepository, 'delete']);
-
-            foreach ($mappingsToCreate as $mapping) {
-                $this->mappingRepository->create($mapping->getPortalNodeKey(), $mergeInto, $mapping->getExternalId());
-            }
-
-            $this->mappingNodeRepository->delete($mergeFrom);
-        } catch (NotFoundException $e) {
-            throw new MappingNodeAreUnmergableException($mergeFrom, $mergeInto, $e);
-        } catch (UnsupportedStorageKeyException $e) {
-            throw new MappingNodeAreUnmergableException($mergeFrom, $mergeInto, $e);
         }
     }
 }
