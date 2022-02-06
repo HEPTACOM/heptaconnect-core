4b3eb96e6a1b516a84ceabdb5cce70fda313f9fa
Joshua Behrens <behrens@heptacom.de>
2022-02-06 18:47:23 +0100

    Remove MappingService::merge
    CONNECT-126

--- Mapping/Contract/MappingServiceInterface.php
+++ Mapping/Contract/MappingServiceInterface.php
@@ -4,7 +4,6 @@ declare(strict_types=1);
 
 namespace Heptacom\HeptaConnect\Core\Mapping\Contract;
 
-use Heptacom\HeptaConnect\Core\Mapping\Exception\MappingNodeAreUnmergableException;
 use Heptacom\HeptaConnect\Portal\Base\StorageKey\Contract\MappingNodeKeyInterface;
 use Heptacom\HeptaConnect\Portal\Base\StorageKey\Contract\PortalNodeKeyInterface;
 
@@ -15,9 +14,4 @@ interface MappingServiceInterface
         MappingNodeKeyInterface $mappingNodeKey,
         \Throwable $exception
     ): void;
-
-    /**
-     * @throws MappingNodeAreUnmergableException
-     */
-    public function merge(MappingNodeKeyInterface $mergeFrom, MappingNodeKeyInterface $mergeInto): void;
 }
