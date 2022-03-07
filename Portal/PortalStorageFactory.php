25e72584cb136420157135bcd5fe415da1348226
Joshua Behrens <behrens@heptacom.de>
2022-03-07 01:04:03 +0100

    Replace list PortalNodeStorage method with storage action
    CONNECT-126

--- Portal/PortalStorageFactory.php
+++ Portal/PortalStorageFactory.php
@@ -10,15 +10,13 @@ use Heptacom\HeptaConnect\Portal\Base\StorageKey\Contract\PortalNodeKeyInterface
 use Heptacom\HeptaConnect\Storage\Base\Contract\Action\PortalNodeStorage\PortalNodeStorageClearActionInterface;
 use Heptacom\HeptaConnect\Storage\Base\Contract\Action\PortalNodeStorage\PortalNodeStorageDeleteActionInterface;
 use Heptacom\HeptaConnect\Storage\Base\Contract\Action\PortalNodeStorage\PortalNodeStorageGetActionInterface;
+use Heptacom\HeptaConnect\Storage\Base\Contract\Action\PortalNodeStorage\PortalNodeStorageListActionInterface;
 use Heptacom\HeptaConnect\Storage\Base\Contract\Action\PortalNodeStorage\PortalNodeStorageSetActionInterface;
-use Heptacom\HeptaConnect\Storage\Base\Contract\PortalStorageContract;
 use Psr\Log\LoggerInterface;
 
 class PortalStorageFactory
 {
     private NormalizationRegistryContract $normalizationRegistry;
-
-    private PortalStorageContract $portalStorage;
 
     private PortalNodeStorageClearActionInterface $portalNodeStorageClearAction;
 
@@ -26,24 +24,26 @@ class PortalStorageFactory
 
     private PortalNodeStorageGetActionInterface $portalNodeStorageGetAction;
 
+    private PortalNodeStorageListActionInterface $portalNodeStorageListAction;
+
     private PortalNodeStorageSetActionInterface $portalNodeStorageSetAction;
 
     private LoggerInterface $logger;
 
     public function __construct(
         NormalizationRegistryContract $normalizationRegistry,
-        PortalStorageContract $portalStorage,
         PortalNodeStorageClearActionInterface $portalNodeStorageClearAction,
         PortalNodeStorageDeleteActionInterface $portalNodeStorageDeleteAction,
         PortalNodeStorageGetActionInterface $portalNodeStorageGetAction,
+        PortalNodeStorageListActionInterface $portalNodeStorageListAction,
         PortalNodeStorageSetActionInterface $portalNodeStorageSetAction,
         LoggerInterface $logger
     ) {
         $this->normalizationRegistry = $normalizationRegistry;
-        $this->portalStorage = $portalStorage;
         $this->portalNodeStorageClearAction = $portalNodeStorageClearAction;
         $this->portalNodeStorageDeleteAction = $portalNodeStorageDeleteAction;
         $this->portalNodeStorageGetAction = $portalNodeStorageGetAction;
+        $this->portalNodeStorageListAction = $portalNodeStorageListAction;
         $this->portalNodeStorageSetAction = $portalNodeStorageSetAction;
         $this->logger = $logger;
     }
@@ -52,10 +52,10 @@ class PortalStorageFactory
     {
         return new PortalStorage(
             $this->normalizationRegistry,
-            $this->portalStorage,
             $this->portalNodeStorageClearAction,
             $this->portalNodeStorageDeleteAction,
             $this->portalNodeStorageGetAction,
+            $this->portalNodeStorageListAction,
             $this->portalNodeStorageSetAction,
             $this->logger,
             $portalNodeKey
