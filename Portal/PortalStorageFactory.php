ea455840220e3e0d683221fd81fc9f4f219e6397
Joshua Behrens <behrens@heptacom.de>
2022-03-19 11:39:37 +0100

    Amend documentation
    CONNECT-126

---
+++ Portal/PortalStorageFactory.php
@@ -0,0 +1,64 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Heptacom\HeptaConnect\Core\Portal;
+
+use Heptacom\HeptaConnect\Portal\Base\Portal\Contract\PortalStorageInterface;
+use Heptacom\HeptaConnect\Portal\Base\Serialization\Contract\NormalizationRegistryContract;
+use Heptacom\HeptaConnect\Portal\Base\StorageKey\Contract\PortalNodeKeyInterface;
+use Heptacom\HeptaConnect\Storage\Base\Contract\Action\PortalNodeStorage\PortalNodeStorageClearActionInterface;
+use Heptacom\HeptaConnect\Storage\Base\Contract\Action\PortalNodeStorage\PortalNodeStorageDeleteActionInterface;
+use Heptacom\HeptaConnect\Storage\Base\Contract\Action\PortalNodeStorage\PortalNodeStorageGetActionInterface;
+use Heptacom\HeptaConnect\Storage\Base\Contract\Action\PortalNodeStorage\PortalNodeStorageListActionInterface;
+use Heptacom\HeptaConnect\Storage\Base\Contract\Action\PortalNodeStorage\PortalNodeStorageSetActionInterface;
+use Psr\Log\LoggerInterface;
+
+class PortalStorageFactory
+{
+    private NormalizationRegistryContract $normalizationRegistry;
+
+    private PortalNodeStorageClearActionInterface $portalNodeStorageClearAction;
+
+    private PortalNodeStorageDeleteActionInterface $portalNodeStorageDeleteAction;
+
+    private PortalNodeStorageGetActionInterface $portalNodeStorageGetAction;
+
+    private PortalNodeStorageListActionInterface $portalNodeStorageListAction;
+
+    private PortalNodeStorageSetActionInterface $portalNodeStorageSetAction;
+
+    private LoggerInterface $logger;
+
+    public function __construct(
+        NormalizationRegistryContract $normalizationRegistry,
+        PortalNodeStorageClearActionInterface $portalNodeStorageClearAction,
+        PortalNodeStorageDeleteActionInterface $portalNodeStorageDeleteAction,
+        PortalNodeStorageGetActionInterface $portalNodeStorageGetAction,
+        PortalNodeStorageListActionInterface $portalNodeStorageListAction,
+        PortalNodeStorageSetActionInterface $portalNodeStorageSetAction,
+        LoggerInterface $logger
+    ) {
+        $this->normalizationRegistry = $normalizationRegistry;
+        $this->portalNodeStorageClearAction = $portalNodeStorageClearAction;
+        $this->portalNodeStorageDeleteAction = $portalNodeStorageDeleteAction;
+        $this->portalNodeStorageGetAction = $portalNodeStorageGetAction;
+        $this->portalNodeStorageListAction = $portalNodeStorageListAction;
+        $this->portalNodeStorageSetAction = $portalNodeStorageSetAction;
+        $this->logger = $logger;
+    }
+
+    public function createPortalStorage(PortalNodeKeyInterface $portalNodeKey): PortalStorageInterface
+    {
+        return new PortalStorage(
+            $this->normalizationRegistry,
+            $this->portalNodeStorageClearAction,
+            $this->portalNodeStorageDeleteAction,
+            $this->portalNodeStorageGetAction,
+            $this->portalNodeStorageListAction,
+            $this->portalNodeStorageSetAction,
+            $this->logger,
+            $portalNodeKey
+        );
+    }
+}
---
+++ Storage/NormalizationRegistry.php
@@ -0,0 +1,65 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Heptacom\HeptaConnect\Core\Storage;
+
+use Heptacom\HeptaConnect\Portal\Base\Serialization\Contract\DenormalizerInterface;
+use Heptacom\HeptaConnect\Portal\Base\Serialization\Contract\NormalizationRegistryContract;
+use Heptacom\HeptaConnect\Portal\Base\Serialization\Contract\NormalizerInterface;
+
+class NormalizationRegistry extends NormalizationRegistryContract
+{
+    /**
+     * @var array|NormalizerInterface[]
+     */
+    private array $normalizer;
+
+    /**
+     * @var array|DenormalizerInterface[]
+     */
+    private array $denormalizer;
+
+    /**
+     * @psalm-param iterable<int, NormalizerInterface> $normalizer
+     * @psalm-param iterable<int, DenormalizerInterface> $denormalizer
+     */
+    public function __construct(iterable $normalizer, iterable $denormalizer)
+    {
+        $this->normalizer = \iterable_to_array($normalizer);
+        $this->denormalizer = \iterable_to_array($denormalizer);
+    }
+
+    public function getNormalizer($value): ?NormalizerInterface
+    {
+        foreach ($this->normalizer as $normalizer) {
+            if ($normalizer->supportsNormalization($value)) {
+                return $normalizer;
+            }
+        }
+
+        return parent::getNormalizer($value);
+    }
+
+    public function getNormalizerByType(string $type): ?NormalizerInterface
+    {
+        foreach ($this->normalizer as $normalizer) {
+            if ($normalizer->getType() === $type) {
+                return $normalizer;
+            }
+        }
+
+        return parent::getNormalizerByType($type);
+    }
+
+    public function getDenormalizer(string $type): ?DenormalizerInterface
+    {
+        foreach ($this->denormalizer as $denormalizer) {
+            if ($denormalizer->getType() === $type) {
+                return $denormalizer;
+            }
+        }
+
+        return parent::getDenormalizer($type);
+    }
+}
