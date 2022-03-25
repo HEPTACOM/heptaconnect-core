f1c507e2c5009258d06c6ee4c94bfb7d3bb3fe40
Julian Krzefski <krzefski@heptacom.de>
2022-03-25 21:02:04 +0100

    CONNECT-25: Add documentation comments

--- Bridge/File/FileRequestUrlProviderInterface.php
+++ Bridge/File/FileRequestUrlProviderInterface.php
@@ -10,7 +10,18 @@ use Psr\Http\Message\UriInterface;
 
 interface FileRequestUrlProviderInterface
 {
-    // TODO: Add token for one-time permission
+    /**
+     * Generates a public URI to retrieve the provided normalized stream. The returned URI **MUST** comply with the
+     * following rules:
+     *
+     * - The URI **SHOULD** point to an HTTP controller of the HEPTAconnect application.
+     * - At the time this URI is returned, a `GET` request without any additional header lines **MUST** respond with the
+     *   contents of the provided normalized stream and use the provided MIME type as `Content-Type` header.
+     * - After this URI is returned, arbitrary conditions **MAY** invalidate it. These conditions **MAY** include (but
+     *   are not limited to):
+     *   - The URI **MAY** expire after a certain time has elapsed.
+     *   - The URI **MAY** expire after it has been accessed a certain number of times.
+     */
     public function resolve(
         PortalNodeKeyInterface $portalNodeKey,
         FileReferenceRequestKeyInterface $requestKey
