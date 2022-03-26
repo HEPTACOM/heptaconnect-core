fa5fbd98ab8f67ffdd8f139367347d8240fbd07c
Julian Krzefski <krzefski@heptacom.de>
2022-03-26 19:33:28 +0000

    Merged in feature/CONNECT-25 (pull request #40)
    File references
    
    Approved-by: Joshua Behrens

---
+++ Web/Http/Contract/HttpHandlerStackBuilderInterface.php
@@ -0,0 +1,21 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Heptacom\HeptaConnect\Core\Web\Http\Contract;
+
+use Heptacom\HeptaConnect\Portal\Base\Web\Http\Contract\HttpHandlerContract;
+use Heptacom\HeptaConnect\Portal\Base\Web\Http\Contract\HttpHandlerStackInterface;
+
+interface HttpHandlerStackBuilderInterface
+{
+    public function push(HttpHandlerContract $httpHandler): self;
+
+    public function pushSource(): self;
+
+    public function pushDecorators(): self;
+
+    public function build(): HttpHandlerStackInterface;
+
+    public function isEmpty(): bool;
+}
