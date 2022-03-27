c339d2bfb6ae95f7860e69928734af2a85e62aad
Julian Krzefski <krzefski@heptacom.de>
2022-03-27 04:26:37 +0200

    CONNECT-317: Use decoration pattern for SerializableCompressNormalizer

--- Storage/Normalizer/SerializableCompressNormalizer.php
+++ Storage/Normalizer/SerializableCompressNormalizer.php
@@ -5,22 +5,35 @@ declare(strict_types=1);
 namespace Heptacom\HeptaConnect\Core\Storage\Normalizer;
 
 use Heptacom\HeptaConnect\Core\Storage\Exception\GzipCompressException;
+use Heptacom\HeptaConnect\Portal\Base\Serialization\Contract\NormalizerInterface;
 
-class SerializableCompressNormalizer extends SerializableNormalizer
+final class SerializableCompressNormalizer implements NormalizerInterface
 {
+    private NormalizerInterface $serializableNormalizer;
+
+    public function __construct(NormalizerInterface $serializableNormalizer)
+    {
+        $this->serializableNormalizer = $serializableNormalizer;
+    }
+
     public function getType(): string
     {
-        return parent::getType() . '+gzpress';
+        return $this->serializableNormalizer->getType() . '+gzpress';
     }
 
     public function normalize($object, $format = null, array $context = [])
     {
-        $result = \gzcompress(parent::normalize($object, $format, $context));
+        $result = \gzcompress($this->serializableNormalizer->normalize($object, $format, $context));
 
         if (!\is_string($result)) {
             throw new GzipCompressException(1637432095);
         }
 
         return $result;
+    }
+
+    public function supportsNormalization($data, $format = null)
+    {
+        return $this->serializableNormalizer->supportsNormalization($data, $format);
     }
 }
