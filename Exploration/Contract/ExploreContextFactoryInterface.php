48d6e7923b0893d5665b0b2cda8c1ea5617d5b49
Joshua Behrens <behrens@heptacom.de>
2022-04-23 23:33:24 +0200

    Change cache key for portal extension stacks

---
+++ Exploration/Contract/ExploreContextFactoryInterface.php
@@ -0,0 +1,13 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Heptacom\HeptaConnect\Core\Exploration\Contract;
+
+use Heptacom\HeptaConnect\Portal\Base\Exploration\Contract\ExploreContextInterface;
+use Heptacom\HeptaConnect\Portal\Base\StorageKey\Contract\PortalNodeKeyInterface;
+
+interface ExploreContextFactoryInterface
+{
+    public function factory(PortalNodeKeyInterface $portalNodeKey): ExploreContextInterface;
+}
