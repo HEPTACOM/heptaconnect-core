71baf2e76574390adbc516a4715d60f08261af5a
Julian Krzefski <krzefski@heptacom.de>
2022-08-15 00:42:36 +0200

    Prevent parallelization lock from being released immediately after creating or checking it

---
+++ Reception/PostProcessing/SaveMappingsPostProcessor.php
@@ -0,0 +1,125 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Heptacom\HeptaConnect\Core\Reception\PostProcessing;
+
+use Heptacom\HeptaConnect\Core\Event\PostReceptionEvent;
+use Heptacom\HeptaConnect\Core\Reception\Contract\PostProcessorContract;
+use Heptacom\HeptaConnect\Core\Reception\Support\PrimaryKeyChangesAttachable;
+use Heptacom\HeptaConnect\Dataset\Base\Contract\DatasetEntityContract;
+use Heptacom\HeptaConnect\Portal\Base\StorageKey\Contract\PortalNodeKeyInterface;
+use Heptacom\HeptaConnect\Portal\Base\Support\Contract\DeepObjectIteratorContract;
+use Heptacom\HeptaConnect\Storage\Base\Action\Identity\Persist\IdentityPersistCreatePayload;
+use Heptacom\HeptaConnect\Storage\Base\Action\Identity\Persist\IdentityPersistDeletePayload;
+use Heptacom\HeptaConnect\Storage\Base\Action\Identity\Persist\IdentityPersistPayload;
+use Heptacom\HeptaConnect\Storage\Base\Action\Identity\Persist\IdentityPersistPayloadCollection;
+use Heptacom\HeptaConnect\Storage\Base\Action\Identity\Persist\IdentityPersistUpdatePayload;
+use Heptacom\HeptaConnect\Storage\Base\Contract\Action\Identity\IdentityPersistActionInterface;
+use Heptacom\HeptaConnect\Storage\Base\PrimaryKeySharingMappingStruct;
+use Psr\Log\LoggerInterface;
+
+final class SaveMappingsPostProcessor extends PostProcessorContract
+{
+    private DeepObjectIteratorContract $deepObjectIterator;
+
+    private IdentityPersistActionInterface $identityPersistAction;
+
+    private LoggerInterface $logger;
+
+    public function __construct(
+        DeepObjectIteratorContract $deepObjectIterator,
+        IdentityPersistActionInterface $identityPersistAction,
+        LoggerInterface $logger
+    ) {
+        $this->deepObjectIterator = $deepObjectIterator;
+        $this->identityPersistAction = $identityPersistAction;
+        $this->logger = $logger;
+    }
+
+    public function handle(PostReceptionEvent $event): void
+    {
+        $saveMappingsData = \iterable_to_array($event->getContext()->getPostProcessingBag()->of(SaveMappingsData::class));
+        $entities = \array_map(static fn (SaveMappingsData $data): DatasetEntityContract => $data->getEntity(), $saveMappingsData);
+
+        $this->saveMappings($event->getContext()->getPortalNodeKey(), \iterable_to_array($entities));
+
+        foreach ($saveMappingsData as $saveMappingData) {
+            $event->getContext()->getPostProcessingBag()->remove($saveMappingData);
+        }
+    }
+
+    /**
+     * @param DatasetEntityContract[] $receivedEntityData
+     */
+    private function saveMappings(PortalNodeKeyInterface $targetPortalNodeKey, array $receivedEntityData): void
+    {
+        if ($receivedEntityData === []) {
+            return;
+        }
+
+        $payload = new IdentityPersistPayload($targetPortalNodeKey, new IdentityPersistPayloadCollection());
+
+        foreach ($this->deepObjectIterator->iterate($receivedEntityData) as $entity) {
+            if (!$entity instanceof DatasetEntityContract) {
+                // no entity
+                continue;
+            }
+
+            $primaryKeyChanges = $entity->getAttachment(PrimaryKeyChangesAttachable::class);
+
+            if (!$primaryKeyChanges instanceof PrimaryKeyChangesAttachable) {
+                // no change
+                continue;
+            }
+
+            $externalId = $primaryKeyChanges->getForeignKey();
+            $firstForeignKey = $primaryKeyChanges->getFirstForeignKey();
+
+            if ($firstForeignKey === $externalId) {
+                // no change
+                continue;
+            }
+
+            $mapping = $entity->getAttachment(PrimaryKeySharingMappingStruct::class);
+
+            if (!$mapping instanceof PrimaryKeySharingMappingStruct) {
+                $this->logger->critical('Unknown mapping origin', [
+                    'code' => 1637527920,
+                    'firstForeignKey' => $firstForeignKey,
+                    'externalId' => $externalId,
+                    'entityType' => \get_class($entity),
+                ]);
+
+                continue;
+            }
+
+            if ($mapping->getExternalId() === null) {
+                $this->logger->critical('Invalid mapping origin', [
+                    'code' => 1637527921,
+                    'firstForeignKey' => $firstForeignKey,
+                    'externalId' => $externalId,
+                    'entityType' => \get_class($entity),
+                ]);
+
+                continue;
+            }
+
+            if ($firstForeignKey === null && $externalId !== null) {
+                $payload->getIdentityPersistPayloads()->push([
+                    new IdentityPersistCreatePayload($mapping->getMappingNodeKey(), $externalId),
+                ]);
+            } elseif ($externalId === null) {
+                $payload->getIdentityPersistPayloads()->push([
+                    new IdentityPersistDeletePayload($mapping->getMappingNodeKey()),
+                ]);
+            } else {
+                $payload->getIdentityPersistPayloads()->push([
+                    new IdentityPersistUpdatePayload($mapping->getMappingNodeKey(), $externalId),
+                ]);
+            }
+        }
+
+        $this->identityPersistAction->persist($payload);
+    }
+}
---
+++ Web/Http/HttpHandlerStackBuilderFactory.php
@@ -0,0 +1,42 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Heptacom\HeptaConnect\Core\Web\Http;
+
+use Heptacom\HeptaConnect\Core\Portal\FlowComponentRegistry;
+use Heptacom\HeptaConnect\Core\Portal\PortalStackServiceContainerFactory;
+use Heptacom\HeptaConnect\Core\Web\Http\Contract\HttpHandlerStackBuilderFactoryInterface;
+use Heptacom\HeptaConnect\Core\Web\Http\Contract\HttpHandlerStackBuilderInterface;
+use Heptacom\HeptaConnect\Portal\Base\StorageKey\Contract\PortalNodeKeyInterface;
+use Heptacom\HeptaConnect\Portal\Base\Web\Http\HttpHandlerCollection;
+use Psr\Log\LoggerInterface;
+
+final class HttpHandlerStackBuilderFactory implements HttpHandlerStackBuilderFactoryInterface
+{
+    private PortalStackServiceContainerFactory $portalContainerFactory;
+
+    private LoggerInterface $logger;
+
+    public function __construct(PortalStackServiceContainerFactory $portalContainerFactory, LoggerInterface $logger)
+    {
+        $this->portalContainerFactory = $portalContainerFactory;
+        $this->logger = $logger;
+    }
+
+    public function createHttpHandlerStackBuilder(
+        PortalNodeKeyInterface $portalNodeKey,
+        string $path
+    ): HttpHandlerStackBuilderInterface {
+        $container = $this->portalContainerFactory->create($portalNodeKey);
+        /** @var FlowComponentRegistry $flowComponentRegistry */
+        $flowComponentRegistry = $container->get(FlowComponentRegistry::class);
+        $components = new HttpHandlerCollection();
+
+        foreach ($flowComponentRegistry->getOrderedSources() as $source) {
+            $components->push($flowComponentRegistry->getWebHttpHandlers($source));
+        }
+
+        return new HttpHandlerStackBuilder($components, $path, $this->logger);
+    }
+}
