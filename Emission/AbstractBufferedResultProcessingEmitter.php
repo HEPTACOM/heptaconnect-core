c890d9ef63fc611fc2e1c13bdd23495066f62c5d
Joshua Behrens <behrens@heptacom.de>
2022-08-25 02:37:41 +0200

    Extract base class for sub-batching emitters
    CONNECT-540

---
+++ Emission/AbstractBufferedResultProcessingEmitter.php
@@ -0,0 +1,97 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Heptacom\HeptaConnect\Core\Emission;
+
+use Heptacom\HeptaConnect\Dataset\Base\Contract\DatasetEntityContract;
+use Heptacom\HeptaConnect\Dataset\Base\EntityType;
+use Heptacom\HeptaConnect\Dataset\Base\TypedDatasetEntityCollection;
+use Heptacom\HeptaConnect\Portal\Base\Emission\Contract\EmitContextInterface;
+use Heptacom\HeptaConnect\Portal\Base\Emission\Contract\EmitterContract;
+use Heptacom\HeptaConnect\Portal\Base\Emission\Contract\EmitterStackInterface;
+
+/**
+ * Base class for emitters that capture results from a stack and process its result in batch size amount of items.
+ */
+abstract class AbstractBufferedResultProcessingEmitter extends EmitterContract
+{
+    private EntityType $entityType;
+
+    private int $batchSize;
+
+    private TypedDatasetEntityCollection $buffer;
+
+    public function __construct(EntityType $entityType, int $batchSize)
+    {
+        $this->entityType = $entityType;
+        $this->batchSize = $batchSize;
+        $this->buffer = $this->createBuffer();
+    }
+
+    public function emit(iterable $externalIds, EmitContextInterface $context, EmitterStackInterface $stack): iterable
+    {
+        try {
+            yield from $this->emitNext($stack, $externalIds, $context);
+        } finally {
+            while ($this->buffer->count() > 0) {
+                $this->dispatchBuffer($context);
+            }
+        }
+    }
+
+    protected function supports(): string
+    {
+        return (string) $this->entityType;
+    }
+
+    protected function extend(DatasetEntityContract $entity, EmitContextInterface $context): DatasetEntityContract
+    {
+        $this->pushBuffer($entity, $this->buffer, $context);
+
+        if ($this->buffer->count() >= $this->batchSize) {
+            $this->dispatchBuffer($context);
+        }
+
+        return $entity;
+    }
+
+    /**
+     * Processes a batch of the buffer. The buffer is not more than batchSize items, but can be less.
+     */
+    abstract protected function processBuffer(
+        TypedDatasetEntityCollection $buffer,
+        EmitContextInterface $context
+    ): void;
+
+    /**
+     * Any data that is returned by the stack is running through this. Here can be filtered what will be buffered.
+     */
+    protected function pushBuffer(
+        DatasetEntityContract $value,
+        TypedDatasetEntityCollection $buffer,
+        EmitContextInterface $context
+    ): void {
+        $buffer->push([$value]);
+    }
+
+    private function dispatchBuffer(EmitContextInterface $context): void
+    {
+        $buffer = $this->buffer;
+        $batchSize = $this->batchSize;
+        $splice = $this->createBuffer();
+
+        for ($step = 0; $step < \max(1, $batchSize) && $buffer->count() > 0; ++$step) {
+            /** @var DatasetEntityContract $item */
+            $item = $buffer->shift();
+            $splice->push([$item]);
+        }
+
+        $this->processBuffer($splice, $context);
+    }
+
+    private function createBuffer(): TypedDatasetEntityCollection
+    {
+        return new TypedDatasetEntityCollection($this->getSupportedEntityType());
+    }
+}
--- Emission/ReceiveJobDispatchingEmitter.php
+++ Emission/ReceiveJobDispatchingEmitter.php
@@ -6,26 +6,15 @@ namespace Heptacom\HeptaConnect\Core\Emission;
 
 use Heptacom\HeptaConnect\Core\Job\Contract\JobDispatcherContract;
 use Heptacom\HeptaConnect\Core\Job\Transition\Contract\EmittedEntitiesToJobsConverterInterface;
-use Heptacom\HeptaConnect\Dataset\Base\Contract\DatasetEntityContract;
-use Heptacom\HeptaConnect\Dataset\Base\DatasetEntityCollection;
 use Heptacom\HeptaConnect\Dataset\Base\EntityType;
 use Heptacom\HeptaConnect\Dataset\Base\TypedDatasetEntityCollection;
 use Heptacom\HeptaConnect\Portal\Base\Emission\Contract\EmitContextInterface;
-use Heptacom\HeptaConnect\Portal\Base\Emission\Contract\EmitterContract;
-use Heptacom\HeptaConnect\Portal\Base\Emission\Contract\EmitterStackInterface;
-use Heptacom\HeptaConnect\Portal\Base\StorageKey\Contract\PortalNodeKeyInterface;
 
-final class ReceiveJobDispatchingEmitter extends EmitterContract
+final class ReceiveJobDispatchingEmitter extends AbstractBufferedResultProcessingEmitter
 {
-    private TypedDatasetEntityCollection $buffer;
-
-    private EntityType $entityType;
-
     private EmittedEntitiesToJobsConverterInterface $emissionResultToJobConverter;
 
     private JobDispatcherContract $jobDispatcher;
-
-    private int $batchSize;
 
     public function __construct(
         EntityType $entityType,
@@ -33,57 +22,15 @@ final class ReceiveJobDispatchingEmitter extends EmitterContract
         JobDispatcherContract $jobDispatcher,
         int $batchSize
     ) {
-        $this->buffer = new TypedDatasetEntityCollection($entityType);
-        $this->entityType = $entityType;
+        parent::__construct($entityType, $batchSize);
+
         $this->emissionResultToJobConverter = $emissionResultToJobConverter;
         $this->jobDispatcher = $jobDispatcher;
-        $this->batchSize = $batchSize;
     }
 
-    public function emit(iterable $externalIds, EmitContextInterface $context, EmitterStackInterface $stack): iterable
+    protected function processBuffer(TypedDatasetEntityCollection $buffer, EmitContextInterface $context): void
     {
-        $this->buffer = new TypedDatasetEntityCollection($this->buffer->getEntityType());
-
-        try {
-            foreach ($this->emitNext($stack, $externalIds, $context) as $key => $value) {
-                yield $key => $value;
-            }
-        } finally {
-            while ($this->buffer->count() > 0) {
-                $this->dispatchBuffer($context->getPortalNodeKey());
-            }
-        }
-    }
-
-    protected function extend(DatasetEntityContract $entity, EmitContextInterface $context): DatasetEntityContract
-    {
-        $this->buffer->push([$entity]);
-
-        if ($this->buffer->count() >= $this->batchSize) {
-            $this->dispatchBuffer($context->getPortalNodeKey());
-        }
-
-        return $entity;
-    }
-
-    protected function supports(): string
-    {
-        return (string) $this->entityType;
-    }
-
-    private function dispatchBuffer(PortalNodeKeyInterface $portalNodeKey): void
-    {
-        $batchSize = $this->batchSize;
-        $buffer = $this->buffer;
-        $entities = [];
-
-        for ($step = 0; $step < $batchSize && $buffer->count() > 0; ++$step) {
-            /** @var DatasetEntityContract $item */
-            $item = $buffer->shift();
-            $entities[] = $item;
-        }
-
-        $jobs = $this->emissionResultToJobConverter->convert($portalNodeKey, new DatasetEntityCollection($entities));
+        $jobs = $this->emissionResultToJobConverter->convert($context->getPortalNodeKey(), $buffer);
         $this->jobDispatcher->dispatch($jobs);
     }
 }
