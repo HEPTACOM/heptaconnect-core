1ec27496cb195a988bf0b4c7ddfb87e537724659
Joshua Behrens <behrens@heptacom.de>
2022-08-25 02:39:50 +0200

    Extract base class for sub-batching explorers
    CONNECT-540

--- Exploration/DirectEmittingExplorer.php
+++ Exploration/DirectEmittingExplorer.php
@@ -5,21 +5,20 @@ declare(strict_types=1);
 namespace Heptacom\HeptaConnect\Core\Exploration;
 
 use Heptacom\HeptaConnect\Core\Emission\Contract\EmitterStackProcessorInterface;
+use Heptacom\HeptaConnect\Dataset\Base\Contract\CollectionInterface;
 use Heptacom\HeptaConnect\Dataset\Base\Contract\DatasetEntityContract;
 use Heptacom\HeptaConnect\Dataset\Base\EntityType;
 use Heptacom\HeptaConnect\Dataset\Base\TypedDatasetEntityCollection;
 use Heptacom\HeptaConnect\Portal\Base\Emission\Contract\EmitContextInterface;
 use Heptacom\HeptaConnect\Portal\Base\Emission\Contract\EmitterStackInterface;
 use Heptacom\HeptaConnect\Portal\Base\Exploration\Contract\ExploreContextInterface;
-use Heptacom\HeptaConnect\Portal\Base\Exploration\Contract\ExplorerContract;
-use Heptacom\HeptaConnect\Portal\Base\Exploration\Contract\ExplorerStackInterface;
-use Heptacom\HeptaConnect\Portal\Base\StorageKey\Contract\PortalNodeKeyInterface;
 use Psr\Log\LoggerInterface;
 
-final class DirectEmittingExplorer extends ExplorerContract
+/**
+ * @extends AbstractBufferedResultProcessingExplorer<DatasetEntityContract>
+ */
+final class DirectEmittingExplorer extends AbstractBufferedResultProcessingExplorer
 {
-    private EntityType $entityType;
-
     private DirectEmitter $directEmitter;
 
     private EmitterStackProcessorInterface $emitterStackProcessor;
@@ -30,8 +29,6 @@ final class DirectEmittingExplorer extends ExplorerContract
 
     private LoggerInterface $logger;
 
-    private int $batchSize;
-
     public function __construct(
         EntityType $entityType,
         DirectEmitter $directEmitter,
@@ -41,82 +38,70 @@ final class DirectEmittingExplorer extends ExplorerContract
         LoggerInterface $logger,
         int $batchSize
     ) {
-        $this->entityType = $entityType;
+        parent::__construct($entityType, $batchSize);
+
         $this->directEmitter = $directEmitter;
         $this->emitterStackProcessor = $emitterStackProcessor;
         $this->emitterStack = $emitterStack;
         $this->emitContext = $emitContext;
         $this->logger = $logger;
-        $this->batchSize = $batchSize;
     }
 
-    public function explore(ExploreContextInterface $context, ExplorerStackInterface $stack): iterable
+    protected function createBuffer(): CollectionInterface
     {
-        $buffer = new TypedDatasetEntityCollection($this->getSupportedEntityType());
-
-        try {
-            foreach ($this->exploreNext($context, $stack) as $key => $value) {
-                if ($value instanceof DatasetEntityContract) {
-                    $primaryKey = $value->getPrimaryKey();
-
-                    if ($primaryKey === null) {
-                        $this->logger->error('DirectEmittingExplorer: Empty or invalid primary key was explored', [
-                            'portalNodeKey' => $context->getPortalNodeKey(),
-                            'entityType' => $this->getSupportedEntityType(),
-                            'primaryKey' => $primaryKey,
-                        ]);
-
-                        continue;
-                    }
-
-                    $this->logger->debug('DirectEmittingExplorer: Entity was explored and job dispatch is prepared', [
-                        'portalNodeKey' => $context->getPortalNodeKey(),
-                        'entityType' => $this->getSupportedEntityType(),
-                        'primaryKey' => $primaryKey,
-                    ]);
-
-                    $buffer->push([$value]);
-
-                    if ($buffer->count() >= $this->batchSize) {
-                        $this->dispatchBuffer($context->getPortalNodeKey(), $buffer);
-                    }
-                }
-
-                yield $key => $value;
-            }
-        } finally {
-            while ($buffer->count() > 0) {
-                $this->dispatchBuffer($context->getPortalNodeKey(), $buffer);
-            }
-        }
+        return new TypedDatasetEntityCollection($this->getSupportedEntityType());
     }
 
-    protected function supports(): string
+    protected function processBuffer(CollectionInterface $buffer, ExploreContextInterface $context): void
     {
-        return (string) $this->entityType;
-    }
-
-    private function dispatchBuffer(PortalNodeKeyInterface $portalNodeKey, TypedDatasetEntityCollection $buffer): void
-    {
-        $batchSize = $this->batchSize;
-        $entities = new TypedDatasetEntityCollection($buffer->getEntityType());
-
-        for ($step = 0; $step < $batchSize && $buffer->count() > 0; ++$step) {
-            /** @var DatasetEntityContract $item */
-            $item = $buffer->shift();
-            $entities->push([$item]);
-        }
-
-        $pks = \iterable_to_array($entities->column('getPrimaryKey'));
+        $pks = \iterable_to_array($buffer->column('getPrimaryKey'));
 
         $this->logger->debug('DirectEmittingExplorer: Flush a batch of direct emissions', [
-            'portalNodeKey' => $portalNodeKey,
+            'portalNodeKey' => $context->getPortalNodeKey(),
             'entityType' => $this->getSupportedEntityType(),
             'primaryKeys' => \implode(', ', $pks),
         ]);
 
         $this->directEmitter->getEntities()->clear();
-        $this->directEmitter->getEntities()->push($entities);
+        $this->directEmitter->getEntities()->push($buffer);
         $this->emitterStackProcessor->processStack($pks, clone $this->emitterStack, $this->emitContext);
+    }
+
+    protected function pushBuffer($value, CollectionInterface $buffer, ExploreContextInterface $context): void
+    {
+        if ($value instanceof DatasetEntityContract) {
+            $this->logger->debug('DirectEmittingExplorer: Entity was explored and job dispatch is prepared', [
+                'portalNodeKey' => $context->getPortalNodeKey(),
+                'entityType' => $this->getSupportedEntityType(),
+                'primaryKey' => $value->getPrimaryKey(),
+            ]);
+
+            $buffer->push([$value]);
+        }
+    }
+
+    protected function isAllowed(string $externalId, ?DatasetEntityContract $entity, ExploreContextInterface $context): bool
+    {
+        if (!parent::isAllowed($externalId, $entity, $context)) {
+            return false;
+        }
+
+        if (!$entity instanceof DatasetEntityContract) {
+            return true;
+        }
+
+        $primaryKey = $entity->getPrimaryKey();
+
+        if ($primaryKey === null) {
+            $this->logger->error('DirectEmittingExplorer: Empty or invalid primary key was explored', [
+                'portalNodeKey' => $context->getPortalNodeKey(),
+                'entityType' => $this->getSupportedEntityType(),
+                'primaryKey' => $primaryKey,
+            ]);
+
+            return false;
+        }
+
+        return true;
     }
 }
--- Exploration/EmissionJobDispatchingExplorer.php
+++ Exploration/EmissionJobDispatchingExplorer.php
@@ -6,25 +6,22 @@ namespace Heptacom\HeptaConnect\Core\Exploration;
 
 use Heptacom\HeptaConnect\Core\Job\Contract\JobDispatcherContract;
 use Heptacom\HeptaConnect\Core\Job\Transition\Contract\ExploredPrimaryKeysToJobsConverterInterface;
+use Heptacom\HeptaConnect\Dataset\Base\Contract\CollectionInterface;
 use Heptacom\HeptaConnect\Dataset\Base\EntityType;
 use Heptacom\HeptaConnect\Dataset\Base\ScalarCollection\StringCollection;
 use Heptacom\HeptaConnect\Portal\Base\Exploration\Contract\ExploreContextInterface;
-use Heptacom\HeptaConnect\Portal\Base\Exploration\Contract\ExplorerContract;
-use Heptacom\HeptaConnect\Portal\Base\Exploration\Contract\ExplorerStackInterface;
-use Heptacom\HeptaConnect\Portal\Base\StorageKey\Contract\PortalNodeKeyInterface;
 use Psr\Log\LoggerInterface;
 
-final class EmissionJobDispatchingExplorer extends ExplorerContract
+/**
+ * @extends AbstractBufferedResultProcessingExplorer<string>
+ */
+final class EmissionJobDispatchingExplorer extends AbstractBufferedResultProcessingExplorer
 {
-    private EntityType $entityType;
-
     private ExploredPrimaryKeysToJobsConverterInterface $exploredPksToJobsConverter;
 
     private JobDispatcherContract $jobDispatcher;
 
     private LoggerInterface $logger;
-
-    private int $batchSize;
 
     public function __construct(
         EntityType $entityType,
@@ -33,65 +30,44 @@ final class EmissionJobDispatchingExplorer extends ExplorerContract
         LoggerInterface $logger,
         int $batchSize
     ) {
-        $this->entityType = $entityType;
+        parent::__construct($entityType, $batchSize);
+
         $this->exploredPksToJobsConverter = $exploredPksToJobsConverter;
         $this->jobDispatcher = $jobDispatcher;
         $this->logger = $logger;
-        $this->batchSize = $batchSize;
     }
 
-    public function explore(ExploreContextInterface $context, ExplorerStackInterface $stack): iterable
+    protected function createBuffer(): CollectionInterface
     {
-        $buffer = new StringCollection();
-
-        try {
-            foreach ($this->exploreNext($context, $stack) as $key => $value) {
-                if (\is_int($value) || \is_string($value)) {
-                    $this->logger->debug('EmissionJobDispatchingExplorer: Entity was explored and job dispatch is prepared', [
-                        'portalNodeKey' => $context->getPortalNodeKey(),
-                        'entityType' => $this->getSupportedEntityType(),
-                        'primaryKey' => (string) $value,
-                    ]);
-
-                    $buffer->push([(string) $value]);
-
-                    if ($buffer->count() >= $this->batchSize) {
-                        $this->dispatchBuffer($context->getPortalNodeKey(), $buffer);
-                    }
-                }
-
-                yield $key => $value;
-            }
-        } finally {
-            while ($buffer->count() > 0) {
-                $this->dispatchBuffer($context->getPortalNodeKey(), $buffer);
-            }
-        }
+        return new StringCollection();
     }
 
-    protected function supports(): string
+    protected function processBuffer(CollectionInterface $buffer, ExploreContextInterface $context): void
     {
-        return (string) $this->entityType;
-    }
-
-    private function dispatchBuffer(PortalNodeKeyInterface $portalNodeKey, StringCollection $buffer): void
-    {
-        $batchSize = $this->batchSize;
-        $pks = new StringCollection();
-
-        for ($step = 0; $step < $batchSize && $buffer->count() > 0; ++$step) {
-            /** @var string $item */
-            $item = $buffer->shift();
-            $pks->push([$item]);
-        }
-
         $this->logger->debug('EmissionJobDispatchingExplorer: Flush a batch of publications', [
-            'portalNodeKey' => $portalNodeKey,
+            'portalNodeKey' => $context->getPortalNodeKey(),
             'entityType' => $this->getSupportedEntityType(),
-            'primaryKeys' => \implode(', ', \iterable_to_array($pks)),
+            'primaryKeys' => \implode(', ', \iterable_to_array($buffer)),
         ]);
 
-        $jobs = $this->exploredPksToJobsConverter->convert($portalNodeKey, $this->getSupportedEntityType(), $pks);
+        $jobs = $this->exploredPksToJobsConverter->convert(
+            $context->getPortalNodeKey(),
+            $this->getSupportedEntityType(),
+            new StringCollection($buffer)
+        );
         $this->jobDispatcher->dispatch($jobs);
+    }
+
+    protected function pushBuffer($value, CollectionInterface $buffer, ExploreContextInterface $context): void
+    {
+        if (\is_int($value) || \is_string($value)) {
+            $this->logger->debug('EmissionJobDispatchingExplorer: Entity was explored and job dispatch is prepared', [
+                'portalNodeKey' => $context->getPortalNodeKey(),
+                'entityType' => $this->getSupportedEntityType(),
+                'primaryKey' => (string) $value,
+            ]);
+
+            $buffer->push([(string) $value]);
+        }
     }
 }
