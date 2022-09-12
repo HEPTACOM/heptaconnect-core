40d8ca4f5892af5cdcc51f75a8fc756768ecdca2
Joshua Behrens <behrens@heptacom.de>
2022-09-12 03:02:09 +0200

    Add audit context
    CONNECT-589

--- Ui/Admin/Action/Context/UiActionContext.php
+++ Ui/Admin/Action/Context/UiActionContext.php
@@ -6,14 +6,23 @@ namespace Heptacom\HeptaConnect\Core\Ui\Admin\Action\Context;
 
 use Heptacom\HeptaConnect\Dataset\Base\AttachmentCollection;
 use Heptacom\HeptaConnect\Dataset\Base\Support\AttachmentAwareTrait;
+use Heptacom\HeptaConnect\Ui\Admin\Base\Audit\UiAuditContext;
 use Heptacom\HeptaConnect\Ui\Admin\Base\Contract\Action\UiActionContextInterface;
 
 final class UiActionContext implements UiActionContextInterface
 {
     use AttachmentAwareTrait;
 
-    public function __construct()
+    private UiAuditContext $auditContext;
+
+    public function __construct(UiAuditContext $auditContext)
     {
         $this->attachments = new AttachmentCollection();
+        $this->auditContext = $auditContext;
+    }
+
+    public function getAuditContext(): UiAuditContext
+    {
+        return $this->auditContext;
     }
 }
