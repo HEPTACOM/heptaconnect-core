062e91658723ac4021eff7ac45f2606ef92d97dd
Joshua Behrens <behrens@heptacom.de>
2022-10-08 11:09:12 +0200

    Move attachment aware
    CONNECT-589

--- Ui/Admin/Action/Context/UiActionContext.php
+++ Ui/Admin/Action/Context/UiActionContext.php
@@ -5,11 +5,12 @@ declare(strict_types=1);
 namespace Heptacom\HeptaConnect\Core\Ui\Admin\Action\Context;
 
 use Heptacom\HeptaConnect\Dataset\Base\AttachmentCollection;
+use Heptacom\HeptaConnect\Dataset\Base\Contract\AttachmentAwareInterface;
 use Heptacom\HeptaConnect\Dataset\Base\Support\AttachmentAwareTrait;
 use Heptacom\HeptaConnect\Ui\Admin\Base\Audit\UiAuditContext;
 use Heptacom\HeptaConnect\Ui\Admin\Base\Contract\Action\UiActionContextInterface;
 
-final class UiActionContext implements UiActionContextInterface
+final class UiActionContext implements AttachmentAwareInterface, UiActionContextInterface
 {
     use AttachmentAwareTrait;
 
