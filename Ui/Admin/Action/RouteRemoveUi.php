a92f901bf4ca9e7bdb2cf07af46f6f910d671b0a
Joshua Behrens <behrens@heptacom.de>
2022-10-13 19:41:29 +0200

    Make route remove compatible with audit trails
    CONNECT-589

--- Ui/Admin/Action/RouteRemoveUi.php
+++ Ui/Admin/Action/RouteRemoveUi.php
@@ -4,36 +4,51 @@ declare(strict_types=1);
 
 namespace Heptacom\HeptaConnect\Core\Ui\Admin\Action;
 
+use Heptacom\HeptaConnect\Core\Ui\Admin\Audit\Contract\AuditTrailFactoryInterface;
 use Heptacom\HeptaConnect\Storage\Base\Action\Route\Delete\RouteDeleteCriteria;
 use Heptacom\HeptaConnect\Storage\Base\Action\Route\Get\RouteGetCriteria;
 use Heptacom\HeptaConnect\Storage\Base\Contract\Action\Route\RouteDeleteActionInterface;
 use Heptacom\HeptaConnect\Storage\Base\Contract\Action\Route\RouteGetActionInterface;
 use Heptacom\HeptaConnect\Storage\Base\Contract\RouteKeyInterface;
 use Heptacom\HeptaConnect\Ui\Admin\Base\Action\Route\RouteRemove\RouteRemoveCriteria;
+use Heptacom\HeptaConnect\Ui\Admin\Base\Action\UiActionType;
 use Heptacom\HeptaConnect\Ui\Admin\Base\Contract\Action\Route\RouteRemoveUiActionInterface;
+use Heptacom\HeptaConnect\Ui\Admin\Base\Contract\Action\UiActionContextInterface;
 use Heptacom\HeptaConnect\Ui\Admin\Base\Contract\Exception\PersistException;
 use Heptacom\HeptaConnect\Ui\Admin\Base\Contract\Exception\RoutesMissingException;
 
 final class RouteRemoveUi implements RouteRemoveUiActionInterface
 {
+    private AuditTrailFactoryInterface $auditTrailFactory;
+
     private RouteGetActionInterface $routeGetAction;
 
     private RouteDeleteActionInterface $routeDeleteAction;
 
-    public function __construct(RouteGetActionInterface $routeGetAction, RouteDeleteActionInterface $routeDeleteAction)
-    {
+    public function __construct(
+        AuditTrailFactoryInterface $auditTrailFactory,
+        RouteGetActionInterface $routeGetAction,
+        RouteDeleteActionInterface $routeDeleteAction
+    ) {
+        $this->auditTrailFactory = $auditTrailFactory;
         $this->routeGetAction = $routeGetAction;
         $this->routeDeleteAction = $routeDeleteAction;
     }
 
-    public function remove(RouteRemoveCriteria $criteria): void
+    public static function class(): UiActionType
     {
+        return new UiActionType(RouteRemoveUiActionInterface::class);
+    }
+
+    public function remove(RouteRemoveCriteria $criteria, UiActionContextInterface $context): void
+    {
+        $trail = $this->auditTrailFactory->create($this, $context->getAuditContext(), [$criteria, $context]);
         $uncheckedRouteKeys = $criteria->getRouteKeys()->asUnique();
 
         try {
             $foundRoutes = $this->routeGetAction->get(new RouteGetCriteria($criteria->getRouteKeys()));
         } catch (\Throwable $throwable) {
-            throw new PersistException(1659293800, $throwable);
+            throw $trail->throwable(new PersistException(1659293800, $throwable));
         }
 
         foreach ($foundRoutes as $foundRoute) {
@@ -43,13 +58,15 @@ final class RouteRemoveUi implements RouteRemoveUiActionInterface
         }
 
         if (!$uncheckedRouteKeys->isEmpty()) {
-            throw new RoutesMissingException($uncheckedRouteKeys, 1659293801);
+            throw $trail->throwable(new RoutesMissingException($uncheckedRouteKeys, 1659293801));
         }
 
         try {
             $this->routeDeleteAction->delete(new RouteDeleteCriteria($criteria->getRouteKeys()));
         } catch (\Throwable $throwable) {
-            throw new PersistException(1659293802, $throwable);
+            throw $trail->throwable(new PersistException(1659293802, $throwable));
         }
+
+        $trail->end();
     }
 }
