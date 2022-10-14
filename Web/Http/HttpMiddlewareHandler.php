6bc4e7097bbced6b81e81e416b424f9b5e47d5ee
Julian Krzefski <krzefski@heptacom.de>
2022-10-14 14:15:33 +0200

    Add HttpMiddlewareHandler to execute PSR-15 middleware chain for inbound http requests

---
+++ Web/Http/HttpMiddlewareHandler.php
@@ -0,0 +1,60 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Heptacom\HeptaConnect\Core\Web\Http;
+
+use Psr\Http\Message\ResponseInterface;
+use Psr\Http\Message\ServerRequestInterface;
+use Psr\Http\Server\MiddlewareInterface;
+use Psr\Http\Server\RequestHandlerInterface;
+
+final class HttpMiddlewareHandler implements RequestHandlerInterface
+{
+    private \Closure $handleStack;
+
+    /**
+     * @var MiddlewareInterface[]
+     */
+    private array $middlewares;
+
+    public function __construct(\Closure $next, MiddlewareInterface ...$middlewares)
+    {
+        $this->handleStack = $next;
+        $this->middlewares = $middlewares;
+    }
+
+    public function handle(ServerRequestInterface $request): ResponseInterface
+    {
+        return $this->next($request, ...$this->middlewares);
+    }
+
+    private function next(ServerRequestInterface $request, MiddlewareInterface ...$middlewares): ResponseInterface
+    {
+        $middleware = \array_shift($middlewares);
+
+        if ($middleware instanceof MiddlewareInterface) {
+            $next = \Closure::fromCallable(function (ServerRequestInterface $request) use ($middlewares) {
+                return $this->next($request, ...$middlewares);
+            });
+
+            $handler = new class($next) implements RequestHandlerInterface {
+                private \Closure $next;
+
+                public function __construct(\Closure $next)
+                {
+                    $this->next = $next;
+                }
+
+                public function handle(ServerRequestInterface $request): ResponseInterface
+                {
+                    return ($this->next)($request);
+                }
+            };
+
+            return $middleware->process($request, $handler);
+        }
+
+        return ($this->handleStack)($request);
+    }
+}
