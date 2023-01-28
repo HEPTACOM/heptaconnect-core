a27c0110b5100931c551f38fd9b381c728250801
Julian Krzefski <krzefski@heptacom.de>
2023-01-28 17:31:17 +0100

    Copy DelegatingLoaderLoadException from core to portal-base and deprecate the old class

--- Portal/Exception/DelegatingLoaderLoadException.php
+++ Portal/Exception/DelegatingLoaderLoadException.php
@@ -4,6 +4,10 @@ declare(strict_types=1);
 
 namespace Heptacom\HeptaConnect\Core\Portal\Exception;
 
+/**
+ * @deprecated Will be removed in version 0.10
+ * Instead use @see \Heptacom\HeptaConnect\Portal\Base\Portal\Exception\DelegatingLoaderLoadException
+ */
 class DelegatingLoaderLoadException extends \Exception
 {
     public function __construct(string $path, ?\Throwable $previous = null)
