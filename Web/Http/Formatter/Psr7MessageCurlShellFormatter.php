4c7802d3358f41351353136d9eb1da4cb2a477e5
Joshua Behrens <behrens@heptacom.de>
2023-01-29 04:04:53 +0100

    Add PSR-7 message formatter implementations
    CONNECT-617

---
+++ Web/Http/Formatter/Psr7MessageCurlShellFormatter.php
@@ -0,0 +1,141 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Heptacom\HeptaConnect\Core\Web\Http\Formatter;
+
+use Heptacom\HeptaConnect\Core\Web\Http\Formatter\Support\HeaderUtility;
+use Heptacom\HeptaConnect\Portal\Base\Web\Http\Contract\Psr7MessageCurlShellFormatterContract;
+use Psr\Http\Message\MessageInterface;
+use Psr\Http\Message\RequestInterface;
+use Psr\Http\Message\ResponseInterface;
+
+final class Psr7MessageCurlShellFormatter extends Psr7MessageCurlShellFormatterContract
+{
+    private HeaderUtility $headerUtility;
+
+    private Psr7MessageRawHttpFormatter $rawFormatter;
+
+    private string $curlCommand;
+
+    public function __construct(
+        HeaderUtility $headerUtility,
+        Psr7MessageRawHttpFormatter $rawFormatter,
+        string $curlCommand
+    ) {
+        $this->headerUtility = $headerUtility;
+        $this->rawFormatter = $rawFormatter;
+        $this->curlCommand = $curlCommand;
+    }
+
+    public function formatMessage(MessageInterface $message): string
+    {
+        if ($message instanceof RequestInterface) {
+            return $this->formatRequest($message);
+        }
+
+        if ($message instanceof ResponseInterface) {
+            return $this->formatResponse($message);
+        }
+
+        throw new \InvalidArgumentException('Message must be a request or a response', 1674950002);
+    }
+
+    public function getFileExtension(MessageInterface $message): string
+    {
+        if ($message instanceof RequestInterface) {
+            return 'sh';
+        }
+
+        if ($message instanceof ResponseInterface) {
+            return $this->rawFormatter->getFileExtension($message);
+        }
+
+        throw new \InvalidArgumentException('Message must be a request or a response', 1674950003);
+    }
+
+    private function formatRequest(RequestInterface $request): string
+    {
+        $request = $this->headerUtility->sortRequestHeaders($request);
+
+        if (\in_array($request->getMethod(), ['GET', 'HEAD', 'OPTIONS', 'TRACE'], true) && $request->hasHeader('content-length')) {
+            $request = $request->withoutHeader('content-length');
+        }
+
+        return $this->curlCommand . ' ' . \implode(' \\' . \PHP_EOL . ' ', $this->getCurlCommandParts($request));
+    }
+
+    private function formatResponse(ResponseInterface $response): string
+    {
+        return $this->rawFormatter->formatMessage($response);
+    }
+
+    private function getCurlCommandParts(RequestInterface $request): array
+    {
+        $commandParts = [];
+        $commandParts[] = (string) $request->getUri();
+        $commandParts[] = '-X ' . $request->getMethod();
+
+        if ($request->getProtocolVersion() === '0.9') {
+            $commandParts[] = '--http0.9';
+        }
+
+        if ($request->getProtocolVersion() === '1.0') {
+            $commandParts[] = '--http1.0';
+        }
+
+        if ($request->getProtocolVersion() === '1.1') {
+            $commandParts[] = '--http1.1';
+        }
+
+        if ($request->getProtocolVersion() === '2.0') {
+            $commandParts[] = '--http2';
+        }
+
+        if ($request->getProtocolVersion() === '3.0') {
+            $commandParts[] = '--http3';
+        }
+
+        foreach ($request->getHeaders() as $header => $values) {
+            if (\in_array(\strtolower($header), ['content-length', 'transfer-encoding'], true)) {
+                continue;
+            }
+
+            $commandParts[] = '-H "' . $header . ': ' . \addcslashes(\implode(', ', $values), '\\"') . '"';
+        }
+
+        if ($request->getBody()->getSize() > 0) {
+            $body = (string) $request->getBody();
+            $encoded = $this->encodeBinaryForCli($body);
+            $commandParts[] = "-d $'" . \addcslashes($encoded, "\\'") . "'";
+        }
+
+        $commandParts[] = '-i';
+        $commandParts[] = '-L';
+        $commandParts[] = '-w "HTTP/%{http_version} %{response_code}"';
+
+        return $commandParts;
+    }
+
+    private function encodeBinaryForCli(string $stream): string
+    {
+        $printables = [
+            ' ',
+            '"',
+            ...\range('a', 'z'),
+            ...\range('A', 'Z'),
+            ...\range('0', '9'),
+        ];
+
+        return \implode('', \array_map(
+            static function (string $byte) use ($printables): string {
+                if (\in_array($byte, $printables, true)) {
+                    return $byte;
+                }
+
+                return '\\x' . \bin2hex($byte);
+            },
+            \str_split($stream)
+        ));
+    }
+}
