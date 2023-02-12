75caab71dfd9d4632faaca56a5126be0e8230d04
Joshua Behrens <behrens@heptacom.de>
2023-02-12 15:19:08 +0100

    Add message pair dumper
    CONNECT-617

---
+++ Web/Http/Dump/RequestResponsePairDumper.php
@@ -0,0 +1,52 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Heptacom\HeptaConnect\Core\Web\Http\Dump;
+
+use Heptacom\HeptaConnect\Core\Bridge\File\HttpHandlerDumpDirectoryPathProviderInterface;
+use Heptacom\HeptaConnect\Core\Web\Http\Contract\HttpHandleServiceInterface;
+use Heptacom\HeptaConnect\Core\Web\Http\Dump\Contract\RequestResponsePairDumperInterface;
+use Heptacom\HeptaConnect\Portal\Base\Web\Http\Contract\Psr7MessageFormatterContract;
+use Heptacom\HeptaConnect\Portal\Base\Web\Http\HttpHandlerStackIdentifier;
+use Psr\Http\Message\ResponseInterface;
+use Psr\Http\Message\ServerRequestInterface;
+
+final class RequestResponsePairDumper implements RequestResponsePairDumperInterface
+{
+    private HttpHandlerDumpDirectoryPathProviderInterface $pathProvider;
+
+    private Psr7MessageFormatterContract $formatter;
+
+    public function __construct(
+        HttpHandlerDumpDirectoryPathProviderInterface $pathProvider,
+        Psr7MessageFormatterContract $formatter
+    ) {
+        $this->pathProvider = $pathProvider;
+        $this->formatter = $formatter;
+    }
+
+    public function dump(
+        HttpHandlerStackIdentifier $httpHandler,
+        ServerRequestInterface $request,
+        ResponseInterface $response
+    ): void {
+        $originalRequest = $request->getAttribute(HttpHandleServiceInterface::REQUEST_ATTRIBUTE_ORIGINAL_REQUEST);
+
+        if ($originalRequest instanceof ServerRequestInterface) {
+            $request = $originalRequest;
+        }
+
+        $correlationId = $response->getHeaderLine('X-HeptaConnect-Correlation-Id');
+        $dumpDir = $this->pathProvider->provide($httpHandler->getPortalNodeKey()) . '-' . $correlationId;
+        $message = $this->formatter->formatMessage($request);
+        $extension = $this->formatter->getFileExtension($request);
+
+        \file_put_contents($dumpDir . '.' . $extension, $message);
+
+        $message = $this->formatter->formatMessage($response);
+        $extension = $this->formatter->getFileExtension($response);
+
+        \file_put_contents($dumpDir . '.' . $extension, $message);
+    }
+}
