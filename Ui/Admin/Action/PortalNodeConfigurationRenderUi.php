36799a128127c2951491e27d2bc7dc668cd4aa71
Joshua Behrens <behrens@heptacom.de>
2023-03-03 01:56:28 +0100

    Use configuration service instead
    CONNECT-410

--- Ui/Admin/Action/PortalNodeConfigurationRenderUi.php
+++ Ui/Admin/Action/PortalNodeConfigurationRenderUi.php
@@ -4,14 +4,14 @@ declare(strict_types=1);
 
 namespace Heptacom\HeptaConnect\Core\Ui\Admin\Action;
 
-use Heptacom\HeptaConnect\Core\Configuration\Contract\PortalNodeConfigurationProcessorServiceInterface;
+use Heptacom\HeptaConnect\Core\Configuration\Contract\ConfigurationServiceInterface;
 use Heptacom\HeptaConnect\Core\Ui\Admin\Audit\Contract\AuditTrailFactoryInterface;
-use Heptacom\HeptaConnect\Ui\Admin\Base\Action\PortalNode\PortalNodeConfigurationGet\PortalNodeConfigurationGetCriteria;
-use Heptacom\HeptaConnect\Ui\Admin\Base\Action\PortalNode\PortalNodeConfigurationGet\PortalNodeConfigurationGetResult;
-use Heptacom\HeptaConnect\Ui\Admin\Base\Action\PortalNode\PortalNodeConfigurationRender\PortalNodeConfigurationRenderResult;
+use Heptacom\HeptaConnect\Core\Ui\Admin\Support\Contract\PortalNodeExistenceSeparatorInterface;
+use Heptacom\HeptaConnect\Portal\Base\StorageKey\Contract\PortalNodeKeyInterface;
+use Heptacom\HeptaConnect\Portal\Base\StorageKey\PortalNodeKeyCollection;
 use Heptacom\HeptaConnect\Ui\Admin\Base\Action\PortalNode\PortalNodeConfigurationRender\PortalNodeConfigurationRenderCriteria;
+use Heptacom\HeptaConnect\Ui\Admin\Base\Action\PortalNode\PortalNodeConfigurationRender\PortalNodeConfigurationRenderResult;
 use Heptacom\HeptaConnect\Ui\Admin\Base\Action\UiActionType;
-use Heptacom\HeptaConnect\Ui\Admin\Base\Contract\Action\PortalNode\PortalNodeConfigurationGetUiActionInterface;
 use Heptacom\HeptaConnect\Ui\Admin\Base\Contract\Action\PortalNode\PortalNodeConfigurationRenderUiActionInterface;
 use Heptacom\HeptaConnect\Ui\Admin\Base\Contract\Action\UiActionContextInterface;
 
@@ -19,8 +19,8 @@ final class PortalNodeConfigurationRenderUi implements PortalNodeConfigurationRe
 {
     public function __construct(
         private AuditTrailFactoryInterface $auditTrailFactory,
-        private PortalNodeConfigurationGetUiActionInterface $pnConfGetUiAction,
-        private PortalNodeConfigurationProcessorServiceInterface $configurationProcessorService
+        private PortalNodeExistenceSeparatorInterface $portalNodeExistenceSeparator,
+        private ConfigurationServiceInterface $configurationService
     ) {
     }
 
@@ -32,16 +32,18 @@ final class PortalNodeConfigurationRenderUi implements PortalNodeConfigurationRe
     public function getRendered(PortalNodeConfigurationRenderCriteria $criteria, UiActionContextInterface $context): iterable
     {
         $trail = $this->auditTrailFactory->create($this, $context->getAuditContext(), [$criteria, $context]);
-        $getCriteria = new PortalNodeConfigurationGetCriteria($criteria->getPortalNodeKeys());
 
-        return $trail->returnIterable(\iterable_map(
-            $this->pnConfGetUiAction->get($getCriteria, $context),
-            fn (PortalNodeConfigurationGetResult $result): PortalNodeConfigurationRenderResult => new PortalNodeConfigurationRenderResult(
-                $result->getPortalNodeKey(),
-                $this->configurationProcessorService->applyRead(
-                    $result->getPortalNodeKey(),
-                    static fn (): array => $result->getConfiguration()
-                )
+        $separation = $this->portalNodeExistenceSeparator->separateKeys($criteria->getPortalNodeKeys());
+        $separation->throwWhenKeysAreMissing($trail);
+
+        $keys = new PortalNodeKeyCollection();
+        $keys->push($separation->getExistingKeys());
+        $keys->push($separation->getPreviewKeys());
+
+        return $trail->returnIterable($keys->map(
+            fn (PortalNodeKeyInterface $portalNodeKey): PortalNodeConfigurationRenderResult => new PortalNodeConfigurationRenderResult(
+                $portalNodeKey,
+                $this->configurationService->getPortalNodeConfiguration($portalNodeKey)
             )
         ));
     }
