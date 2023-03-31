cfdfd55999147afa4acffa117f928f599e8c46f9
Julian Krzefski <krzefski@heptacom.de>
2023-03-31 21:08:09 +0200

    Parse raw request-body in HttpKernel

--- Web/Http/HttpKernel.php
+++ Web/Http/HttpKernel.php
@@ -9,6 +9,9 @@ use Heptacom\HeptaConnect\Portal\Base\StorageKey\Contract\PortalNodeKeyInterface
 use Heptacom\HeptaConnect\Portal\Base\Web\Http\Contract\HttpKernelInterface;
 use Psr\Http\Message\ResponseInterface;
 use Psr\Http\Message\ServerRequestInterface;
+use Psr\Http\Message\StreamFactoryInterface;
+use Psr\Http\Message\UploadedFileFactoryInterface;
+use Riverline\MultiPartParser\Converters\PSR7 as MultiPartParser;
 
 final class HttpKernel implements HttpKernelInterface
 {
@@ -16,18 +19,27 @@ final class HttpKernel implements HttpKernelInterface
 
     private HttpHandleServiceInterface $httpHandleService;
 
+    private StreamFactoryInterface $streamFactory;
+
+    private UploadedFileFactoryInterface $uploadedFileFactory;
+
     public function __construct(
         PortalNodeKeyInterface $portalNodeKey,
-        HttpHandleServiceInterface $httpHandleService
+        HttpHandleServiceInterface $httpHandleService,
+        StreamFactoryInterface $streamFactory,
+        UploadedFileFactoryInterface $uploadedFileFactory
     ) {
         $this->portalNodeKey = $portalNodeKey;
         $this->httpHandleService = $httpHandleService;
+        $this->streamFactory = $streamFactory;
+        $this->uploadedFileFactory = $uploadedFileFactory;
     }
 
     public function handle(ServerRequestInterface $request): ResponseInterface
     {
         $request = $this->getRequestWithQueryParams($request);
         $request = $this->getRequestWithCookieParams($request);
+        $request = $this->getRequestWithParsedBodyAndUploadedFiles($request);
 
         return $this->httpHandleService->handle($request, $this->portalNodeKey);
     }
@@ -58,5 +70,71 @@ final class HttpKernel implements HttpKernelInterface
         \parse_str($request->getUri()->getQuery(), $queryParams);
 
         return $request->withQueryParams($queryParams);
+    }
+
+    private function getRequestWithParsedBodyAndUploadedFiles(ServerRequestInterface $request): ServerRequestInterface
+    {
+        $contentType = $request->getHeaderLine('Content-Type');
+
+        if (\str_starts_with($contentType, 'multipart/form-data')) {
+            $document = MultiPartParser::convert($request);
+
+            $parsedBodyIndex = 0;
+            $parsedBody = [];
+
+            $uploadedFilesIndex = 0;
+            $uploadedFiles = [];
+
+            foreach ($document->getParts() as $part) {
+                $name = $part->getName();
+                $body = $part->getBody();
+
+                if ($part->isFile()) {
+                    $fileName = $part->getFileName();
+                    $mimeType = $part->getMimeType();
+
+                    $uploadedFiles[$name ?? $uploadedFilesIndex++] = $this->uploadedFileFactory->createUploadedFile(
+                        $this->streamFactory->createStream($body),
+                        null,
+                        \UPLOAD_ERR_OK,
+                        $fileName,
+                        $mimeType
+                    );
+                } else {
+                    $parsedBody[$name ?? $parsedBodyIndex++] = $body;
+                }
+            }
+
+            if ($parsedBody !== []) {
+                $request = $request->withParsedBody($parsedBody);
+            }
+
+            if ($uploadedFiles !== []) {
+                $request = $request->withUploadedFiles($uploadedFiles);
+            }
+        } elseif (\str_starts_with($contentType, 'application/json')) {
+            try {
+                $parsedBody = \json_decode(
+                    (string) $request->getBody(),
+                    true,
+                    512,
+                    \JSON_THROW_ON_ERROR
+                );
+            } catch (\JsonException $_) {
+                $parsedBody = null;
+            }
+
+            if (\is_array($parsedBody)) {
+                $request = $request->withParsedBody($parsedBody);
+            }
+        } elseif (\str_starts_with($contentType, 'application/x-www-form-urlencoded')) {
+            \parse_str((string) $request->getBody(), $parsedBody);
+
+            if (\is_array($parsedBody) && $parsedBody !== []) {
+                $request = $request->withParsedBody($parsedBody);
+            }
+        }
+
+        return $request;
     }
 }
