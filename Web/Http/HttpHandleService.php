9afbe984f065fc30a0d247e0f93d072444b4c732
Julian Krzefski <krzefski@heptacom.de>
2023-05-27 19:34:10 +0200

    Refactor HttpHandleService to always generate a stack

--- Web/Http/HttpHandleService.php
+++ Web/Http/HttpHandleService.php
@@ -20,6 +20,7 @@ use Heptacom\HeptaConnect\Portal\Base\Web\Http\ServerRequestCycle;
 use Heptacom\HeptaConnect\Storage\Base\Action\WebHttpHandlerConfiguration\Find\WebHttpHandlerConfigurationFindCriteria;
 use Heptacom\HeptaConnect\Storage\Base\Contract\Action\WebHttpHandlerConfiguration\WebHttpHandlerConfigurationFindActionInterface;
 use Heptacom\HeptaConnect\Storage\Base\Contract\StorageKeyGeneratorContract;
+use Psr\Http\Message\RequestInterface;
 use Psr\Http\Message\ResponseFactoryInterface;
 use Psr\Http\Message\ResponseInterface;
 use Psr\Http\Message\ServerRequestInterface;
@@ -29,7 +30,7 @@ use Ramsey\Uuid\Uuid;
 final class HttpHandleService implements HttpHandleServiceInterface
 {
     /**
-     * @var array<array-key, HttpHandlerStackInterface|null>
+     * @var array<array-key, HttpHandlerStackInterface>
      */
     private array $stackCache = [];
 
@@ -128,31 +129,24 @@ final class HttpHandleService implements HttpHandleServiceInterface
 
             $response = $response->withStatus(423);
         } else {
-            $stack = $this->getStack($stackIdentifier);
+            $stack = $this->getStack($stackIdentifier, $request, $correlationId);
 
-            if (!$stack instanceof HttpHandlerStackInterface) {
-                $this->logger->critical(LogMessage::WEB_HTTP_HANDLE_NO_HANDLER_FOR_PATH(), [
-                    'code' => 1636845086,
-                    'path' => $stackIdentifier->getPath(),
-                    'portalNodeKey' => $stackIdentifier->getPortalNodeKey(),
-                    'request' => $request,
-                    'web_http_correlation_id' => $correlationId,
-                ]);
-            } else {
-                $response = $this->actor->performHttpHandling(
-                    $request,
-                    $response,
-                    $stack,
-                    $this->getContext($stackIdentifier->getPortalNodeKey())
-                );
-            }
+            $response = $this->actor->performHttpHandling(
+                $request,
+                $response,
+                $stack,
+                $this->getContext($stackIdentifier->getPortalNodeKey())
+            );
         }
 
         return $response->withHeader('X-HeptaConnect-Correlation-Id', $correlationId);
     }
 
-    private function getStack(HttpHandlerStackIdentifier $identifier): ?HttpHandlerStackInterface
-    {
+    private function getStack(
+        HttpHandlerStackIdentifier $identifier,
+        RequestInterface $request,
+        string $correlationId
+    ): HttpHandlerStackInterface {
         $cacheKey = $this->storageKeyGenerator->serialize($identifier->getPortalNodeKey()->withoutAlias()) . $identifier->getPath();
 
         if (!\array_key_exists($cacheKey, $this->stackCache)) {
@@ -161,9 +155,21 @@ final class HttpHandleService implements HttpHandleServiceInterface
                 ->pushSource()
                 ->pushDecorators();
 
+            $isStackEmpty = $builder->isEmpty();
+
+            if ($isStackEmpty) {
+                $this->logger->notice(LogMessage::WEB_HTTP_HANDLE_NO_HANDLER_FOR_PATH(), [
+                    'code' => 1636845086,
+                    'path' => $identifier->getPath(),
+                    'portalNodeKey' => $identifier->getPortalNodeKey(),
+                    'request' => $request,
+                    'web_http_correlation_id' => $correlationId,
+                ]);
+            }
+
             $builder->push(new HttpMiddlewareChainHandler(
                 $identifier->getPath(),
-                $builder->isEmpty()
+                $isStackEmpty
             ));
 
             $this->stackCache[$cacheKey] = $builder->build();
@@ -171,11 +177,7 @@ final class HttpHandleService implements HttpHandleServiceInterface
 
         $result = $this->stackCache[$cacheKey];
 
-        if ($result instanceof HttpHandlerStackInterface) {
-            return clone $result;
-        }
-
-        return null;
+        return clone $result;
     }
 
     private function getContext(PortalNodeKeyInterface $portalNodeKey): HttpHandleContextInterface
