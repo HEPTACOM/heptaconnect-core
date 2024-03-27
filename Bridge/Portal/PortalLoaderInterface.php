e1aee15b098a162b386cff4a7c0cb61095f3e7b2
Julian Krzefski <krzefski@heptacom.de>
2024-03-27 15:33:37 +0100

    Add documentation for PortalLoaderInterface

--- Bridge/Portal/PortalLoaderInterface.php
+++ Bridge/Portal/PortalLoaderInterface.php
@@ -7,9 +7,18 @@ namespace Heptacom\HeptaConnect\Core\Bridge\Portal;
 use Heptacom\HeptaConnect\Portal\Base\Portal\PortalCollection;
 use Heptacom\HeptaConnect\Portal\Base\Portal\PortalExtensionCollection;
 
+/**
+ * Loads available portals and portal-extensions
+ */
 interface PortalLoaderInterface
 {
+    /**
+     * Finds classes for available portals and constructs objects from those classes
+     */
     public function getPortals(): PortalCollection;
 
+    /**
+     * Finds classes for available portal-extensions and constructs objects from those classes
+     */
     public function getPortalExtensions(): PortalExtensionCollection;
 }
