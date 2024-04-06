0bd347c81efe2e12f2af4b74dcd3656e8e5fdf6d
Joshua Behrens <behrens@heptacom.de>
2024-04-06 16:54:04 +0200

    Move AttachableInterface
    CONNECT-579

--- Reception/Support/LockAttachable.php
+++ Reception/Support/LockAttachable.php
@@ -4,7 +4,7 @@ declare(strict_types=1);
 
 namespace Heptacom\HeptaConnect\Core\Reception\Support;
 
-use Heptacom\HeptaConnect\Dataset\Base\Contract\AttachableInterface;
+use Heptacom\HeptaConnect\Utility\Contract\AttachableInterface;
 use Symfony\Component\Lock\LockInterface;
 
 final class LockAttachable implements AttachableInterface
